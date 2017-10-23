3d918e6cea2599673cf236335b0919f2bce9aaac
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2017-10-22 21:40:31 -0400

    Update to Apache license headers and add a license check (#99)
    Signed-off-by: vvvprabhakar <vvvprabhakar@gmail.com>

---
+++ scripts/check-license.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+licRes=$(
+for file in $(find src -type f -iname '*.js' ! -path '*/thrift_gen/*'); do
+	head -n3 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || echo "  ${file}"
+done;)
+if [ -n "${licRes}" ]; then
+	echo "license header check failed:\n${licRes}"
+	exit 255
+fi
