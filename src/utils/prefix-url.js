5520c89d9b84f9e86e8413f578158e2780bb8807
Joe Farro <joef@uber.com>
2018-03-18 19:51:14 -0400

    Fix #42 use <base> and config webpack at runtime
    Signed-off-by: Joe Farro <joef@uber.com>

--- src/utils/prefix-url.js
+++ src/utils/prefix-url.js
@@ -12,44 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { homepage } from '../../package.json';
+import sitePrefix from '../site-prefix';
 
-// strip the domain, first slash and trailing slash (if present)
-const rx = new RegExp('^(?:https?://[^/]+)?/?(.*?)/?$', 'i');
-let prefix = '';
-
+const origin = process.env.NODE_ENV === 'test' ? global.location.origin : window.location.origin;
 /**
- * Generate the URL prefix from `value` and use it for all subsequent calls to
- * `prefixUrl()`. All of the following `value` parameters result in the prefix
- * `"/some/prefix"` (although, the first is the preferred form).
+ * Generate the URL prefix from `sitePrefix` and use it for all subsequent calls
+ * to `prefixUrl()`. `sitePrefix` should be an absolute URL, e.g. with an origin.
+ * `pathPrefix` is just the path portion and should not have a trailing slash:
  *
- * - `"/some/prefix"`
- * - `"some/prefix"`
- * - `"/some/prefix/"`
- * - `"http://my.example.com/some/prefix"`
- * - `"https://my.example.com/some/prefix/"`
- *
- * Note: This function has a side effect of setting the default URL prefix
- * applied via the file's default explort.
- *
- * @param {?string} value The value to derive the URL prefix from.
- * @return {string} The resultant prefix.
+ * - `"http://localhost:3000/"` to `""`
+ * - `"http://localhost:3000/abc/"` to `"/abc"`
+ * - `"http://localhost:3000/abc/def/"` to `"/abc/def"`
  */
-export function deriveAndSetPrefix(value) {
-  prefix = value ? value.replace(rx, '/$1') : '';
-  return prefix;
-}
-
-deriveAndSetPrefix(homepage);
+const rx = new RegExp(`^${origin}|/$`, 'ig');
+const pathPrefix = sitePrefix.replace(rx, '');
 
 /**
- * Add the URL prefix, derived from `homepage` in `package.json`, to the URL
- * argument. The domain is stripped from `homepage` before being added.
+ * Add the path prefix to the  URL. See [site-prefix.js](../site-prefix.js) and
+ * the `<base>` tag in [index.html](../../public/index.html) for details.
  *
  * @param {string} value The URL to have the prefix added to.
  * @return {string} The resultant URL.
  */
 export default function prefixUrl(value) {
   const s = value == null ? '' : String(value);
-  return prefix + s;
+  return `${pathPrefix}${s}`;
 }
