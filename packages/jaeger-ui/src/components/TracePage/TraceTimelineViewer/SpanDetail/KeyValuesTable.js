5f34760344d56a1f7fb74a3eb0d64b3d58e36c24
Everett Ross <reverett@uber.com>
2018-12-19 17:02:40 -0500

    Add a tooltip to copy icon in KeyValuesTable
    Signed-off-by: Everett Ross <reverett@uber.com>

--- packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/KeyValuesTable.js
+++ packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/KeyValuesTable.js
@@ -16,7 +16,7 @@
 
 import * as React from 'react';
 import jsonMarkup from 'json-markup';
-import { Dropdown, Icon, Menu } from 'antd';
+import { Dropdown, Icon, Menu, Tooltip } from 'antd';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import type { KeyValuePair, Link } from '../../../../types/trace';
 
@@ -33,7 +33,7 @@ function parseIfJson(value) {
   return value;
 }
 
-const LinkValue = (props: { href: string, title?: string, children: React.Node }) => (
+export const LinkValue = (props: { href: string, title?: string, children: React.Node }) => (
   <a href={props.href} title={props.title} target="_blank" rel="noopener noreferrer">
     {props.children} <Icon className="KeyValueTable--linkIcon" type="export" />
   </a>
@@ -56,59 +56,104 @@ type KeyValuesTableProps = {
   linksGetter: ?(KeyValuePair[], number) => Link[],
 };
 
-export default function KeyValuesTable(props: KeyValuesTableProps) {
-  const { data, linksGetter } = props;
-  return (
-    <div className="KeyValueTable u-simple-scrollbars">
-      <table className="u-width-100">
-        <tbody className="KeyValueTable--body">
-          {data.map((row, i) => {
-            const markup = {
-              __html: jsonMarkup(parseIfJson(row.value)),
-            };
-            // eslint-disable-next-line react/no-danger
-            const jsonTable = <div className="ub-inline-block" dangerouslySetInnerHTML={markup} />;
-            const links = linksGetter ? linksGetter(data, i) : null;
-            let valueMarkup;
-            if (links && links.length === 1) {
-              valueMarkup = (
-                <div>
-                  <LinkValue href={links[0].url} title={links[0].text}>
-                    {jsonTable}
-                  </LinkValue>
-                </div>
-              );
-            } else if (links && links.length > 1) {
-              valueMarkup = (
-                <div>
-                  <Dropdown overlay={linkValueList(links)} placement="bottomRight" trigger={['click']}>
-                    <a>
-                      {jsonTable} <Icon className="KeyValueTable--linkIcon" type="profile" />
-                    </a>
-                  </Dropdown>
-                </div>
-              );
-            } else {
-              valueMarkup = jsonTable;
-            }
-            return (
-              // `i` is necessary in the key because row.key can repeat
-              // eslint-disable-next-line react/no-array-index-key
-              <tr key={`${row.key}-${i}`}>
-                <td className="KeyValueTable--keyColumn">{row.key}</td>
-                <td>{valueMarkup}</td>
-                <td className="KeyValueTable--copyColumn">
-                  <CopyToClipboard text={JSON.stringify(row, null, 2)}>
-                    <Icon className="KeyValueTable--copyIcon" type="copy" />
-                  </CopyToClipboard>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+type KeyValuesTableState = {
+  copiedRows: Set<KeyValuePair>,
+};
 
-KeyValuesTable.LinkValue = LinkValue;
+export default class KeyValuesTable extends React.PureComponent<KeyValuesTableProps, KeyValuesTableState> {
+  props: KeyValuesTableProps;
+
+  constructor(props: KeyValuesTableProps) {
+    super(props);
+
+    this.state = {
+      copiedRows: new Set(),
+    };
+  }
+
+  handleCopyIconClick = (row: KeyValuePair) => {
+    const newCopiedRows = new Set(this.state.copiedRows);
+    newCopiedRows.add(row);
+    this.setState({
+      copiedRows: newCopiedRows,
+    });
+  };
+
+  handleTooltipVisibilityChange = (row: KeyValuePair, visible: boolean) => {
+    if (!visible && this.state.copiedRows.has(row)) {
+      const newCopiedRows = new Set(this.state.copiedRows);
+      newCopiedRows.delete(row);
+      this.setState({
+        copiedRows: newCopiedRows,
+      });
+    }
+  };
+
+  render() {
+    const { data, linksGetter } = this.props;
+    return (
+      <div className="KeyValueTable u-simple-scrollbars">
+        <table className="u-width-100">
+          <tbody className="KeyValueTable--body">
+            {data.map((row, i) => {
+              const tooltipTitle = this.state.copiedRows.has(row) ? 'Copied' : 'Copy JSON';
+              const markup = {
+                __html: jsonMarkup(parseIfJson(row.value)),
+              };
+              // eslint-disable-next-line react/no-danger
+              const jsonTable = <div className="ub-inline-block" dangerouslySetInnerHTML={markup} />;
+              const links = linksGetter ? linksGetter(data, i) : null;
+              let valueMarkup;
+              if (links && links.length === 1) {
+                valueMarkup = (
+                  <div>
+                    <LinkValue href={links[0].url} title={links[0].text}>
+                      {jsonTable}
+                    </LinkValue>
+                  </div>
+                );
+              } else if (links && links.length > 1) {
+                valueMarkup = (
+                  <div>
+                    <Dropdown overlay={linkValueList(links)} placement="bottomRight" trigger={['click']}>
+                      <a>
+                        {jsonTable} <Icon className="KeyValueTable--linkIcon" type="profile" />
+                      </a>
+                    </Dropdown>
+                  </div>
+                );
+              } else {
+                valueMarkup = jsonTable;
+              }
+              return (
+                // `i` is necessary in the key because row.key can repeat
+                // eslint-disable-next-line react/no-array-index-key
+                <tr key={`${row.key}-${i}`}>
+                  <td className="KeyValueTable--keyColumn">{row.key}</td>
+                  <td>{valueMarkup}</td>
+                  <td className="KeyValueTable--copyColumn">
+                    <Tooltip
+                      arrowPointAtCenter
+                      mouseLeaveDelay={0.5}
+                      onVisibleChange={visible => this.handleTooltipVisibilityChange(row, visible)}
+                      placement={'left'}
+                      title={tooltipTitle}
+                    >
+                      <CopyToClipboard text={JSON.stringify(row, null, 2)}>
+                        <Icon
+                          className="KeyValueTable--copyIcon"
+                          onClick={() => this.handleCopyIconClick(row)}
+                          type="copy"
+                        />
+                      </CopyToClipboard>
+                    </Tooltip>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
