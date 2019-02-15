0bf673b7dcc0ff08bbeb69d4fa47afb61d166433
Joe Farro <joef@uber.com>
2019-02-15 18:16:25 -0500

    Fix formatting issue
    Signed-off-by: Joe Farro <joef@uber.com>

--- packages/jaeger-ui/src/utils/redux-form-field-adapter.js
+++ packages/jaeger-ui/src/utils/redux-form-field-adapter.js
@@ -20,7 +20,7 @@ import * as React from 'react';
 
 import './redux-form-field-adapter.css';
 
-const noop = () => { };
+const noop = () => {};
 
 export default function reduxFormFieldAdapter({
   AntInputComponent,
