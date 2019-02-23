ca0454c86bc5c8048f91d9e1c97bffd9890d241d
Ruben Vargas Palma <ruben.vp8510@gmail.com>
2019-02-23 04:34:40 -0500

    Hide child status icon on SpanTreeOffset used in SpanDetailRow component (#334)
    * Hide child status icon on SpanTreeOffset used in SpanDetailRow component
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    * Small tweak to flow type
    
    Signed-off-by: Joe Farro <joef@uber.com>
    
    * Rename showChildrenStatus attribute from SpanTreeOffset to showChildrenIcon
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetailRow.js
+++ packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetailRow.js
@@ -65,7 +65,7 @@ export default class SpanDetailRow extends React.PureComponent<SpanDetailRowProp
     return (
       <TimelineRow className="detail-row">
         <TimelineRow.Cell width={columnDivision}>
-          <SpanTreeOffset span={span} />
+          <SpanTreeOffset span={span} showChildrenIcon={false} />
           <span>
             <span
               className="detail-row-expanded-accent"
