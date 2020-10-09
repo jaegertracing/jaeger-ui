00fdec4805b5a72bd5a75b3338f43e90c80fc862
Tim Klever <tim@timisawesome.com>
2020-10-08 19:46:00 -0700

    Adds additional test coverage around common component <ErrorMessage> (#625)
    Signed-off-by: Tim Klever <Tim.V.Klever@aexp.com>
    
    Co-authored-by: Ruben Vargas Palma <ruben.vp8510@gmail.com>

--- packages/jaeger-ui/src/components/common/ErrorMessage.test.js
+++ packages/jaeger-ui/src/components/common/ErrorMessage.test.js
@@ -37,6 +37,35 @@ describe('<ErrorMessage>', () => {
     expect(msg.shallow().text()).toMatch(error);
   });
 
+  it('<Details /> renders empty on string error', () => {
+    wrapper = shallow(<ErrorMessage.Details error={error} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('<Details /> renders wrapper on wrap', () => {
+    error = {
+      message: 'some-http-ish-message',
+      httpStatusText: 'value-httpStatusText',
+      httpUrl: 'value-httpUrl',
+      httpQuery: 'value-httpQuery',
+      httpBody: 'value-httpBody',
+    };
+    wrapper = shallow(<ErrorMessage.Details error={error} wrap />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('<Details /> renders custom wrapper class', () => {
+    error = {
+      message: 'some-http-ish-message',
+      httpStatusText: 'value-httpStatusText',
+      httpUrl: 'value-httpUrl',
+      httpQuery: 'value-httpQuery',
+      httpBody: 'value-httpBody',
+    };
+    wrapper = shallow(<ErrorMessage.Details error={error} wrap wrapperClassName="TEST-WRAPPER-CLASS" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   describe('rendering more complex errors', () => {
     it('renders the error message', () => {
       error = new Error('another-error');
@@ -66,6 +95,53 @@ describe('<ErrorMessage>', () => {
         const errorAttr = detailsWrapper.find(`ErrorAttr[value="${error[key]}"]`);
         expect(errorAttr.length).toBe(1);
       });
+    });
+
+    it('renders truncated body excerpt on large body', () => {
+      error = {
+        message: 'some-http-ish-message',
+        httpStatus: 'value-httpStatus',
+        httpStatusText: 'value-httpStatusText',
+        httpUrl: 'value-httpUrl',
+        httpQuery: 'value-httpQuery',
+        httpBody: `value-httpBody-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel
+          arcu mattis, pellentesque metus eget, fermentum dolor. Pellentesque habitant morbi
+          tristique senectus et netus et malesuada fames ac turpis egestas. Integer placerat turpis
+          diam, in eleifend est congue maximus. Sed dictum ipsum a dolor pellentesque, vestibulum
+          congue mi pharetra. Pellentesque id augue sit amet libero consectetur rutrum ac rutrum
+          nisl. Proin suscipit, libero et gravida tristique, magna dolor pulvinar ex, sit amet
+          accumsan tellus metus vel elit. Ut condimentum ultrices felis vel feugiat. Praesent tellus
+          nisl, feugiat ut est sed, lobortis lacinia elit. Aenean consequat nunc arcu, in tincidunt
+          libero blandit non.
+          
+          Vestibulum aliquet lobortis tincidunt. Donec in nunc a sapien rutrum ultricies eu ac
+          felis. Vestibulum feugiat in mauris id fringilla. Nulla enim nisl, rutrum vel tellus vel,
+          tincidunt aliquam enim. Nullam in tortor mi. Vestibulum pellentesque velit eget eros
+          varius, ut sollicitudin urna consectetur. Ut gravida, dolor id varius faucibus, magna
+          lorem condimentum metus, sit amet hendrerit nisi nisi eu dolor. Nulla vel metus sit amet
+          odio rhoncus rutrum eu in sapien. Mauris aliquam sed sem sit amet dignissim.`,
+      };
+
+      wrapper.setProps({ error });
+      const details = wrapper.find('Details');
+      expect(details.length).toBe(1);
+      const detailsWrapper = details.shallow();
+      expect(detailsWrapper).toMatchSnapshot();
+    });
+
+    it('renders on missing httpStatus from the error', () => {
+      error = {
+        message: 'some-http-ish-message',
+        httpStatusText: 'value-httpStatusText',
+        httpUrl: 'value-httpUrl',
+        httpQuery: 'value-httpQuery',
+        httpBody: 'value-httpBody',
+      };
+      wrapper.setProps({ error });
+      const details = wrapper.find('Details');
+      expect(details.length).toBe(1);
+      const detailsWrapper = details.shallow();
+      expect(detailsWrapper).toMatchSnapshot();
     });
   });
 
