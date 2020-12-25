ed0ca859c77441dc663ec4fc6a318f86d4351af8
Yuri Shkuro <github@ysh.us>
2020-12-25 14:03:47 -0500

    Display references unless it's a single CHILD_OF
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/index.tsx
+++ packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/index.tsx
@@ -140,7 +140,8 @@ export default function SpanDetail(props: SpanDetailProps) {
             onToggle={() => warningsToggle(spanID)}
           />
         )}
-        {references && references.length > 0 &&
+        {references &&
+          references.length > 0 &&
           (references.length > 1 || references[0].refType !== 'CHILD_OF') && (
             <AccordianReferences
               data={references}
