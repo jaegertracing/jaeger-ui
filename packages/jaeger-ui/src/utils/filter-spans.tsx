e97bf81eab45a3a314e67203b7335b35e77927f8
vvvprabhakar <vvvprabhakar@gmail.com>
2021-07-05 03:14:37 +0530

    signoff
    Signed-off-by: vvvprabhakar <vvvprabhakar@gmail.com>

--- packages/jaeger-ui/src/utils/filter-spans.tsx
+++ packages/jaeger-ui/src/utils/filter-spans.tsx
@@ -57,7 +57,7 @@ export default function filterSpans(textFilter: string, spans: Span[] | TNil) {
     isTextInFilters(includeFilters, span.operationName) ||
     isTextInFilters(includeFilters, span.process.serviceName) ||
     isTextInKeyValues(span.tags) ||
-    span.logs.some(log => isTextInKeyValues(log.fields)) ||
+    (span.logs !== null && span.logs.some(log => isTextInKeyValues(log.fields))) ||
     isTextInKeyValues(span.process.tags) ||
     includeFilters.some(filter => filter.replace(/^0*/, '') === span.spanID.replace(/^0*/, ''));
 
