bcf9b44c872bb63ee339fe7240c9440aa4573782
Yuri Shkuro <github@ysh.us>
2022-11-13 10:52:21 -0500

    delint
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- packages/jaeger-ui/src/setupProxy.js
+++ packages/jaeger-ui/src/setupProxy.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function setupProxy(app) {
