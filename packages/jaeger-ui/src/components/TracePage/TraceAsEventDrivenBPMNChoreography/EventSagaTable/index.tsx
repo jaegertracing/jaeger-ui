1606537f2d12da261cca116c8f4bb5bb4ed7f1b8
gerbeu <eugen.gerb@hs-furtwangen.de>
2023-02-12 13:07:59 +0100

    add

---
+++ packages/jaeger-ui/src/components/TracePage/TraceAsEventDrivenBPMNChoreography/EventSagaTable/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './index.css';
+
+const EventSagaTable = ({trace} : any) => {
+
+  return (
+    <div className="card">
+      Event Saga
+    </div>
+  );
+};
+
+export default EventSagaTable;
