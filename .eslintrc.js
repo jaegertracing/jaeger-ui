ec3e08931aea4948e2a0c134b42ba2c60d4782fd
Máté Szabó <mszabo@fandom.com>
2023-02-27 22:26:28 +0100

    Use .jsx file extension for JS components (#1218)
    ## Which problem is this PR solving?
    * Split from https://github.com/jaegertracing/jaeger-ui/pull/1212
    
    ## Short description of the changes
    This is to ease an eventual migration to Vite, since Vite by default
    only supports JSX in files with the .jsx or .tsx extensions, and this is
    surprisingly hard to override. Update the Prettier and ESLint
    configurations accordingly.
    
    Signed-off-by: Máté Szabó <mszabo@fandom.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', 'json', '.tsx'],
+        extensions: ['.js', '.jsx', 'json', '.tsx'],
       },
     },
   },
