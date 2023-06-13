aeb24162681ebee544be1b62d0fc4c7a7db78dbd
Yuri Shkuro <ysh@meta.com>
2023-06-13 13:56:32 -0400

    more-lines
    Signed-off-by: Yuri Shkuro <ysh@meta.com>

--- scripts/check-license.sh
+++ scripts/check-license.sh
@@ -2,7 +2,7 @@
 
 licRes=$(
 for file in $(find scripts packages/*/src packages/*/test packages/plexus/demo -type f -iregex '.*\.[cjt]ss*x*$' \! -name 'layout.worker.bundled.js'); do
-	head -n3 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || echo "  ${file}"
+	head -n6 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || echo "  ${file}"
 done;)
 if [ -n "${licRes}" ]; then
 	echo "license header check failed:\n${licRes}"
