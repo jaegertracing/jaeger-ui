85f9b8223d94f843bfbf34d668572db99e6bbb75
Yuri Shkuro <ysh@meta.com>
2023-06-16 22:28:46 -0400

    linter
    Signed-off-by: Yuri Shkuro <ysh@meta.com>

--- packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/KeyValuesTable.test.js
+++ packages/jaeger-ui/src/components/TracePage/TraceTimelineViewer/SpanDetail/KeyValuesTable.test.js
@@ -152,7 +152,7 @@ describe('<KeyValuesTable>', () => {
     const copyIcons = wrapper.find(CopyIcon);
     expect(copyIcons.length).toBe(2 * data.length); // Copy and Copy JSON buttons
     copyIcons.forEach((copyIcon, i) => {
-      let datum = data[Math.floor(i / 2)];
+      const datum = data[Math.floor(i / 2)];
       if (i % 2 == 0) {
         expect(copyIcon.prop('copyText')).toBe(datum.value);
         expect(copyIcon.prop('tooltipTitle')).toBe('Copy value');
