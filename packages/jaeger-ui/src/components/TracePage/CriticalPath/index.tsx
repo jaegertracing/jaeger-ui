29dc91896f2259fcfd7b35933248ba0c8ef08a92
GLVS Kiriti <glvskiriti2003369@gmail.com>
2023-07-01 23:35:54 +0530

    ComputeCriticalPath function can find critical path
    Here in criticalPath array  all critical Path sections of each span are added to it
    
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/CriticalPath/index.tsx
+++ packages/jaeger-ui/src/components/TracePage/CriticalPath/index.tsx
@@ -1,39 +1,100 @@
-import React from "react";
-import { Span, Trace } from "../../../types/trace";
+import { Span, Trace } from '../../../types/trace';
 
 type Tprops = {
-    trace: Trace
-}
+  trace: Trace;
+};
 
-const findRootSpanId = (spans: Span[]):string | undefined => {
-    const rootSpan = spans.find((span)=>span.references.length === 0 && span.depth === 0);
-    return rootSpan?.spanID
-}
+// It is a section of span that lies on critical path
+type criticalPathSection = {
+  spanId: string;
+  section_start: number;
+  section_end: number;
+};
 
-const findChildSpanIds = (spans: Span[]): Span[] => {
-    const refinedSpanData:Span[] = []
-    spans.forEach((span)=>{
-        if(span.hasChildren){
-            const Children = spans.filter((span2)=>span2.references.some((reference)=>reference.refType === 'CHILD_OF' && reference.spanID === span.spanID)).map((span2)=>span2.spanID);
-            refinedSpanData.push({...span,childSpanIds: Children})
-        }
-        else{
-            refinedSpanData.push({...span,childSpanIds: []})
-        }
-    })
+function TraceCriticalPath(props: Tprops) {
+  let traceData: Trace;
+  const criticalPath: criticalPathSection[] = [];
+
+  const findRootSpanId = (spans: Span[]): string | undefined => {
+    const rootSpan = spans.find(span => span.references.length === 0 && span.depth === 0);
+    return rootSpan?.spanID;
+  };
+
+  // This function finds child spans for each span
+  const findChildSpanIds = (spans: Span[]): Span[] => {
+    const refinedSpanData: Span[] = [];
+    spans.forEach(span => {
+      if (span.hasChildren) {
+        const Children = spans
+          .filter(span2 =>
+            span2.references.some(
+              reference => reference.refType === 'CHILD_OF' && reference.spanID === span.spanID
+            )
+          )
+          .sort((a, b) => b.startTime + b.duration - (a.startTime + a.duration))
+          .map(span2 => span2.spanID);
+        refinedSpanData.push({ ...span, childSpanIds: Children });
+      } else {
+        refinedSpanData.push({ ...span, childSpanIds: [] });
+      }
+    });
     return refinedSpanData;
-}
+  };
 
-
-function TraceCriticalPath(props: Tprops ){
-    console.log(props.trace);
-    const RootSpanId = findRootSpanId(props.trace.spans);
-    if(RootSpanId){
-        const refinedSpanData:Span[] = findChildSpanIds(props.trace.spans);
-        console.log("Refined Span Data is");
-        console.log(refinedSpanData);
+  const computeCriticalPath = (spanId: string, spawnTime?: number) => {
+    // spawnTime refers to spawn synchronization event of current span
+    const currentSpan: Span = traceData.spans.filter(span => span.spanID === spanId)[0];
+    let lastFinishingChildSpanId: string | undefined;
+    let spanCriticalSection: criticalPathSection;
+    if (spawnTime) {
+      lastFinishingChildSpanId = currentSpan.childSpanIds.find(each => {
+        return (
+          traceData.spans.filter(span => span.spanID === each && span.startTime + span.duration < spawnTime)
+            .length > 0
+        );
+      });
+    } else {
+      lastFinishingChildSpanId = currentSpan.childSpanIds[0];
     }
-    return null
+
+    if (lastFinishingChildSpanId) {
+      const lfcSpan = traceData.spans.filter(span => span.spanID === lastFinishingChildSpanId)[0];
+      spanCriticalSection = {
+        spanId: currentSpan.spanID,
+        section_start: lfcSpan.startTime + lfcSpan.duration,
+        section_end: spawnTime || currentSpan.startTime + currentSpan.duration,
+      };
+      criticalPath.push(spanCriticalSection);
+      computeCriticalPath(lastFinishingChildSpanId);
+    } else {
+      // If there is no last finishing child then total section upto startTime of span is on critical path
+      spanCriticalSection = {
+        spanId: currentSpan.spanID,
+        section_start: currentSpan.startTime,
+        section_end: spawnTime || currentSpan.startTime + currentSpan.duration,
+      };
+      criticalPath.push(spanCriticalSection);
+      // Now as there are no lfc's it goes back to parent span from startTime(spawn Event)
+      if (currentSpan.references.length) {
+        const parentSpan: string = currentSpan.references.filter(
+          reference => reference.refType === 'CHILD_OF'
+        )[0].spanID;
+        computeCriticalPath(parentSpan, currentSpan.startTime);
+      }
+    }
+  };
+
+  traceData = props.trace;
+  const rootSpanId = findRootSpanId(props.trace.spans);
+  // If there is root span then algorithm implements
+  if (rootSpanId) {
+    const refinedSpanData: Span[] = findChildSpanIds(props.trace.spans);
+    traceData = { ...traceData, spans: refinedSpanData };
+    computeCriticalPath(rootSpanId);
+    console.log(criticalPath);
+    // Now you got the critical path sections
+  }
+  return null;
 }
 
-export default TraceCriticalPath
+export default TraceCriticalPath;
