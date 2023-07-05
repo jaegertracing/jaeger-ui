34775837b256a3a71bf5419b94729a40736eb3e4
GLVS Kiriti <glvskiriti2003369@gmail.com>
2023-07-05 17:55:49 +0530

    Licenses added to new files
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/CriticalPath/testResults/test1.js
+++ packages/jaeger-ui/src/components/TracePage/CriticalPath/testResults/test1.js
@@ -1,46 +1,63 @@
-export const criticalPathSections = [{
-    "spanId": "150c193cf155b46b",
-    "section_start": 1579070675086319,
-    "section_end": 1579070675087902,
-},
-{
-    "spanId": "22be69e72cbcde0b",
-    "section_start": 1579070675085939,
-    "section_end": 1579070675086319,
-},
-{
-    "spanId": "e821b549888cbc3b",
-    "section_start": 1579070675085892,
-    "section_end": 1579070675085939,
-},
-{
-    "spanId": "622a5079b565623e",
-    "section_start": 1579070675085840,
-    "section_end": 1579070675085892,
-},
-{
-    "spanId": "e821b549888cbc3b",
-    "section_start": 1579070675085080,
-    "section_end": 1579070675085840,
-},
-{
-    "spanId": "4085dddb47429851",
-    "section_start": 1579070675085059,
-    "section_end": 1579070675085080,
-},
-{
-    "spanId": "e821b549888cbc3b",
-    "section_start": 1579070675084200,
-    "section_end": 1579070675085059,
-},
-{
-    "spanId": "22be69e72cbcde0b",
-    "section_start": 1579070675083820,
-    "section_end": 1579070675084200,
-},
-{
-    "spanId": "150c193cf155b46b",
-    "section_start": 1579070675083000,
-    "section_end": 1579070675083820,
-},
-]
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const criticalPathSections = [
+  {
+    spanId: '150c193cf155b46b',
+    section_start: 1579070675086319,
+    section_end: 1579070675087902,
+  },
+  {
+    spanId: '22be69e72cbcde0b',
+    section_start: 1579070675085939,
+    section_end: 1579070675086319,
+  },
+  {
+    spanId: 'e821b549888cbc3b',
+    section_start: 1579070675085892,
+    section_end: 1579070675085939,
+  },
+  {
+    spanId: '622a5079b565623e',
+    section_start: 1579070675085840,
+    section_end: 1579070675085892,
+  },
+  {
+    spanId: 'e821b549888cbc3b',
+    section_start: 1579070675085080,
+    section_end: 1579070675085840,
+  },
+  {
+    spanId: '4085dddb47429851',
+    section_start: 1579070675085059,
+    section_end: 1579070675085080,
+  },
+  {
+    spanId: 'e821b549888cbc3b',
+    section_start: 1579070675084200,
+    section_end: 1579070675085059,
+  },
+  {
+    spanId: '22be69e72cbcde0b',
+    section_start: 1579070675083820,
+    section_end: 1579070675084200,
+  },
+  {
+    spanId: '150c193cf155b46b',
+    section_start: 1579070675083000,
+    section_end: 1579070675083820,
+  },
+];
+
+export default criticalPathSections;
