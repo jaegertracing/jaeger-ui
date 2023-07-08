4c55b72956bf520a086e11078c0ca82d8b161b8c
GLVS Kiriti <glvskiriti2003369@gmail.com>
2023-07-08 23:24:58 +0530

    Unit test added for a overflwing span case
    In this case child span is entirely outside the parent span so it is dropped
    
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/CriticalPath/index.test.js
+++ packages/jaeger-ui/src/components/TracePage/CriticalPath/index.test.js
@@ -16,29 +16,33 @@ import TraceCriticalPath, {
   findRootSpanId,
   findChildSpanIds,
   findLastFinishingChildSpanId,
+  sanitizeOverFlowingChildren,
   computeCriticalPath,
 } from './index';
 import test1 from './testResults/test1';
 import test2 from './testResults/test2';
+import test3 from './testResults/test3';
 
-describe.each([[test1], [test2]])('findRootSpanId', testProps => {
+describe.each([[test1], [test2], [test3]])('findRootSpanId', testProps => {
   it('Should find RootSpanId correctly', () => {
     const rootSpanId = findRootSpanId(testProps.trace.spans);
     expect(rootSpanId).toBe(testProps.rootSpanId);
   });
 });
 
-describe.each([[test1], [test2]])('findChildSpanIds', testProps => {
+describe.each([[test1], [test2], [test3]])('findChildSpanIds', testProps => {
   it('Should find child spanIds correctly and also in sortorder of endTime', () => {
-    const refinedSpanData = findChildSpanIds(testProps.trace.spans);
-    expect(refinedSpanData.length).toBe(5);
+    const sanitizedData = sanitizeOverFlowingChildren(testProps.trace.spans);
+    const refinedSpanData = findChildSpanIds(sanitizedData);
+    expect(refinedSpanData.length).toBe(testProps.refinedSpanData.length);
     expect(refinedSpanData).toStrictEqual(testProps.refinedSpanData);
   });
 });
 
-describe.each([[test1], [test2]])('findLastFinishingChildSpanId', testProps => {
+describe.each([[test1], [test2], [test3]])('findLastFinishingChildSpanId', testProps => {
   it('Should find lfc of a span correctly', () => {
-    const refinedSpanData = findChildSpanIds(testProps.trace.spans);
+    const sanitizedData = sanitizeOverFlowingChildren(testProps.trace.spans);
+    const refinedSpanData = findChildSpanIds(sanitizedData);
     const traceData = { ...testProps.trace, spans: refinedSpanData };
 
     let currentSpan = traceData.spans.filter(span => span.spanID === testProps.lfcInputSpan)[0];
@@ -52,9 +56,10 @@ describe.each([[test1], [test2]])('findLastFinishingChildSpanId', testProps => {
   });
 });
 
-describe.each([[test1], [test2]])('Happy Path', testProps => {
+describe.each([[test1], [test2], [test3]])('Happy Path', testProps => {
   it('Should find criticalPathSections correctly', () => {
-    const refinedSpanData = findChildSpanIds(testProps.trace.spans);
+    const sanitizedData = sanitizeOverFlowingChildren(testProps.trace.spans);
+    const refinedSpanData = findChildSpanIds(sanitizedData);
     const traceData = { ...testProps.trace, spans: refinedSpanData };
     const criticalPath = computeCriticalPath(traceData, testProps.rootSpanId, []);
     expect(criticalPath).toStrictEqual(testProps.criticalPathSections);
---
+++ packages/jaeger-ui/src/components/TracePage/CriticalPath/testResults/test3.js
@@ -0,0 +1,57 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import transformTraceData from '../../../../model/transform-trace-data';
+/*
+
+      x┌──────────┐x
+      x│ Span A   │x
+      x└──────────┘x
+      xxxxxxxxxxxxxx     ┌─────────────────────────────────────┐
+                         │              Span B                 │
+                         └─────────────────────────────────────┘
+
+
+Span B will be dropped and span A is on critical path
+*/
+
+const trace = require('../../TraceStatistics/tableValuesTestTrace/traceWithSingleChildSpanLongerThanParent.json');
+
+const transformedTrace = transformTraceData(trace);
+
+const sanitizedSpanData = [transformedTrace.spans[0]];
+sanitizedSpanData[0].childSpanIds = [];
+
+const refinedSpanData = sanitizedSpanData;
+
+const criticalPathSections = [
+  {
+    spanId: '006c3cf93508f205',
+    section_start: 1679437737490189,
+    section_end: 1679437737490225,
+  },
+];
+
+const test3 = {
+  criticalPathSections,
+  trace: transformedTrace,
+  refinedSpanData,
+  rootSpanId: '006c3cf93508f205',
+  lfcInputSpan: '006c3cf93508f205',
+  lfc: undefined,
+  lfcWithSpawnTime: undefined,
+  spawnTime: undefined,
+};
+
+export default test3;
