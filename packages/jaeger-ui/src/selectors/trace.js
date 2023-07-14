09e88f333630c0357ae7d120695288a9a6057256
GLVS Kiriti <glvskiriti2003369@gmail.com>
2023-07-14 09:52:48 +0530

    Comparator now sorts based on endTime
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- packages/jaeger-ui/src/selectors/trace.js
+++ packages/jaeger-ui/src/selectors/trace.js
@@ -51,7 +51,7 @@ export function getTraceSpanIdsAsTree(trace) {
   const comparator = (nodeA, nodeB) => {
     const a = spansById.get(nodeA.value);
     const b = spansById.get(nodeB.value);
-    return +(a.startTime > b.startTime) || +(a.startTime === b.startTime) - 1;
+    return (a, b) => b.startTime + b.duration - (a.startTime + a.duration);
   };
   trace.spans.forEach(span => {
     const node = nodesById.get(span.spanID);
