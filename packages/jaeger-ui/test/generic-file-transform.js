6d53b7552bc604e01e34fb155138d751339b8279
Rajat Khanduri <rajatkhanduri290102@gmail.com>
2023-08-14 02:18:09 +0530

    Remove unused src parameter in process method
    Signed-off-by: Rajat Khanduri <rajatkhanduri290102@gmail.com>

--- packages/jaeger-ui/test/generic-file-transform.js
+++ packages/jaeger-ui/test/generic-file-transform.js
@@ -16,7 +16,7 @@ const path = require('path');
 
 // Simple Jest transform that stubs out CSS files and returns the file name as the mock content for other file types.
 module.exports = {
-  process(src, filename) {
+  process(filename) {
     const fileExtension = path.extname(filename);
     if (fileExtension === 'css') {
       return { code: 'module.exports = "";' };
