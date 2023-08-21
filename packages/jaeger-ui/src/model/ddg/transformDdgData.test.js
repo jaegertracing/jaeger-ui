6cb3083f7763650297b217537292afd9973fa650
Ansh Goyal <anshgoyal1704@gmail.com>
2023-08-21 20:13:38 +0530

    change isEqual import
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- packages/jaeger-ui/src/model/ddg/transformDdgData.test.js
+++ packages/jaeger-ui/src/model/ddg/transformDdgData.test.js
@@ -15,7 +15,7 @@
 import _filter from 'lodash/filter';
 import _flatten from 'lodash/flatten';
 import _map from 'lodash/map';
-import _ from 'lodash';
+import _isEqual from 'lodash/isEqual';
 
 import transformDdgData from './transformDdgData';
 import * as testResources from './sample-paths.test.resources';
@@ -171,7 +171,7 @@ describe('transform ddg data', () => {
     const simpleModel = transformDdgData(wrap([simplePath, longSimplePath]), focalPayloadElem);
     const reverseModel = transformDdgData(wrap([longSimplePath, simplePath]), focalPayloadElem);
 
-    expect(_.isEqual(reverseModel, simpleModel)).toBe(false);
+    expect(_isEqual(reverseModel, simpleModel)).toBe(false);
     expect(reverseModel).not.toBe(simpleModel);
     expect(reverseModel.hash).toBe(simpleModel.hash);
 
