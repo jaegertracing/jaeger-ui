db7b8aaeed2d9f185d313db017b85c0e92054f9b
Ansh Goyal <anshgoyal1704@gmail.com>
2023-08-25 16:47:24 +0530

    mock window.matchMedia for other files also
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- packages/jaeger-ui/src/components/Monitor/ServicesView/operationDetailsTable/index.test.js
+++ packages/jaeger-ui/src/components/Monitor/ServicesView/operationDetailsTable/index.test.js
@@ -28,6 +28,22 @@ const props = {
   hoveredRowKey: [],
 };
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
 describe('<OperationTableDetails>', () => {
   let wrapper;
 
--- packages/jaeger-ui/src/components/TracePage/TraceSpanView/index.test.js
+++ packages/jaeger-ui/src/components/TracePage/TraceSpanView/index.test.js
@@ -21,6 +21,22 @@ const testTrace = require('../TraceStatistics/tableValuesTestTrace/testTrace.jso
 
 const transformedTrace = transformTraceData(testTrace);
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
 describe('<TraceSpanView>', () => {
   let wrapper;
   let defaultProps;
