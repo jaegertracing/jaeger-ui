4467206357d5c94d5328361234264c561dbdd2d6
GLVS Kiriti <glvskiriti2003369@gmail.com>
2023-09-14 12:35:09 +0530

    Reduced further nesting and modified to suggested changes
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/CriticalPath/utils/sanitizeOverFlowingChildren.tsx
+++ packages/jaeger-ui/src/components/TracePage/CriticalPath/utils/sanitizeOverFlowingChildren.tsx
@@ -32,66 +32,71 @@ const sanitizeOverFlowingChildren = (spanMap: Map<string, Span>): Map<string, Sp
     }
     // parentSpan will be undefined when its parentSpan is dropped previously
     const parentSpan = spanMap.get(span.references[0].spanID);
-    if (parentSpan) {
-      const childEndTime = span.startTime + span.duration;
-      const parentEndTime = parentSpan.startTime + parentSpan.duration;
-      if (span.startTime >= parentSpan.startTime) {
-        if (span.startTime >= parentEndTime) {
-          // child outside of parent range => drop the child span
-          //      |----parent----|
-          //                        |----child--|
-          // Remove the childSpan from spanMap
-          spanMap.delete(span.spanID);
 
-          // Remove the childSpanId from its parent span
-          parentSpan.childSpanIds = parentSpan.childSpanIds.filter(id => id === span.spanID);
-          spanMap.set(parentSpan.spanID, { ...parentSpan });
-        } else if (childEndTime > parentEndTime) {
-            // child end after parent, truncate is needed
-            //      |----parent----|
-            //              |----child--|
-            spanMap.set(span.spanID, {
-              ...span,
-              duration: parentEndTime - span.startTime,
-            });
-        }else{
-          // everything looks good
-          // |----parent----|
-          //   |----child--|
-        }
-      } else if (childEndTime <= parentSpan.startTime) {
-          // child outside of parent range => drop the child span
-          //                      |----parent----|
-          //       |----child--|
-
-          // Remove the childSpan from spanMap
-          spanMap.delete(span.spanID);
-
-          // Remove the childSpanId from its parent span
-          parentSpan.childSpanIds = parentSpan.childSpanIds.filter(id => id === span.spanID);
-          spanMap.set(parentSpan.spanID, { ...parentSpan });
-        } else if (childEndTime <= parentEndTime) {
-            // child start before parent, truncate is needed
-            //      |----parent----|
-            //   |----child--|
-            spanMap.set(span.spanID, {
-              ...span,
-              startTime: parentSpan.startTime,
-              duration: childEndTime - parentSpan.startTime,
-            });
-          } else {
-            // child start before parent and end after parent, truncate is needed
-            //      |----parent----|
-            //  |---------child---------|
-            spanMap.set(span.spanID, {
-              ...span,
-              startTime: parentSpan.startTime,
-              duration: parentEndTime - parentSpan.startTime,
-            });
-          }
-    } else {
+    if (!parentSpan) {
       // Drop the child spans of dropped parent span
       spanMap.delete(span.spanID);
+      return;
+    }
+    const childEndTime = span.startTime + span.duration;
+    const parentEndTime = parentSpan.startTime + parentSpan.duration;
+    if (span.startTime >= parentSpan.startTime) {
+      if (span.startTime >= parentEndTime) {
+        // child outside of parent range => drop the child span
+        //      |----parent----|
+        //                        |----child--|
+        // Remove the childSpan from spanMap
+        spanMap.delete(span.spanID);
+
+        // Remove the childSpanId from its parent span
+        parentSpan.childSpanIds = parentSpan.childSpanIds.filter(id => id === span.spanID);
+        spanMap.set(parentSpan.spanID, { ...parentSpan });
+        return;
+      }
+      if (childEndTime > parentEndTime) {
+        // child end after parent, truncate is needed
+        //      |----parent----|
+        //              |----child--|
+        spanMap.set(span.spanID, {
+          ...span,
+          duration: parentEndTime - span.startTime,
+        });
+        return;
+      }
+      // everything looks good
+      // |----parent----|
+      //   |----child--|
+      return;
+    }
+    if (childEndTime <= parentSpan.startTime) {
+      // child outside of parent range => drop the child span
+      //                      |----parent----|
+      //       |----child--|
+
+      // Remove the childSpan from spanMap
+      spanMap.delete(span.spanID);
+
+      // Remove the childSpanId from its parent span
+      parentSpan.childSpanIds = parentSpan.childSpanIds.filter(id => id === span.spanID);
+      spanMap.set(parentSpan.spanID, { ...parentSpan });
+    } else if (childEndTime <= parentEndTime) {
+      // child start before parent, truncate is needed
+      //      |----parent----|
+      //   |----child--|
+      spanMap.set(span.spanID, {
+        ...span,
+        startTime: parentSpan.startTime,
+        duration: childEndTime - parentSpan.startTime,
+      });
+    } else {
+      // child start before parent and end after parent, truncate is needed
+      //      |----parent----|
+      //  |---------child---------|
+      spanMap.set(span.spanID, {
+        ...span,
+        startTime: parentSpan.startTime,
+        duration: parentEndTime - parentSpan.startTime,
+      });
     }
   });
 
