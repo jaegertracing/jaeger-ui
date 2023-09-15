46c5a1467819c6088b98526518cfabb6b7ebb47b
suhail34 <suhailkna@gmail.com>
2023-09-16 01:47:30 +0530

    Making lint check pass
    Ran yarn prettier command to make the lint test pass
    
    Signed-off-by: suhail34 <suhailkna@gmail.com>

--- packages/jaeger-ui/src/components/App/TopNav.test.js
+++ packages/jaeger-ui/src/components/App/TopNav.test.js
@@ -12,18 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import { render, screen, fireEvent, getByText } from '@testing-library/react'
-import { BrowserRouter, Router } from 'react-router-dom'
-import '@testing-library/jest-dom'
+import React from 'react';
+import { render, screen, getByText } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
 
-import { mapStateToProps, TopNavImpl as TopNav } from './TopNav'
+import { mapStateToProps, TopNavImpl as TopNav } from './TopNav';
 
 describe('<TopNav>', () => {
-  const labelGitHub = 'GitHub'
-  const githubUrl = 'https://github.com/uber/jaeger'
-  const blogUrl = 'https://medium.com/jaegertracing/'
-  const labelAbout = 'About Jaeger'
+  const labelGitHub = 'GitHub';
+  const githubUrl = 'https://github.com/uber/jaeger';
+  const blogUrl = 'https://medium.com/jaegertracing/';
+  const labelAbout = 'About Jaeger';
   const dropdownItems = [
     {
       label: 'Version 1',
@@ -37,12 +37,12 @@ describe('<TopNav>', () => {
       url: 'https://twitter.com/JaegerTracing',
       anchorTarget: '_self',
     },
-  ]
+  ];
 
   const configMenuGroup = {
     label: labelAbout,
     items: dropdownItems,
-  }
+  };
 
   const defaultProps = {
     config: {
@@ -67,108 +67,108 @@ describe('<TopNav>', () => {
       location: { location: { pathname: 'some-path' } },
     },
     traceDiff: {},
-  }
+  };
 
   describe('renders the default menu options', () => {
-    let component
+    let component;
     beforeEach(() => {
       component = render(
         <BrowserRouter>
           <TopNav {...defaultProps} />
         </BrowserRouter>
-      )
-    })
+      );
+    });
 
     afterEach(() => {
-      component.unmount()
-    })
+      component.unmount();
+    });
 
     it('renders the "JAEGER UI" link', () => {
-      const items = screen.getByRole('link', { name: 'JAEGER UI' })
-      expect(items).toBeInTheDocument()
-    })
+      const items = screen.getByRole('link', { name: 'JAEGER UI' });
+      expect(items).toBeInTheDocument();
+    });
 
     it('renders the "Search" button', () => {
-      const items = screen.getByRole('link', { name: 'Search' })
-      expect(items).toBeInTheDocument()
-    })
+      const items = screen.getByRole('link', { name: 'Search' });
+      expect(items).toBeInTheDocument();
+    });
 
     it('renders the "System Architecture" button', () => {
-      const items = screen.getByRole('link', { name: 'System Architecture' })
-      expect(items).toBeInTheDocument()
-    })
-  })
+      const items = screen.getByRole('link', { name: 'System Architecture' });
+      expect(items).toBeInTheDocument();
+    });
+  });
 
   describe('renders the custom menu', () => {
-    let component
+    let component;
     beforeEach(() => {
       component = render(
         <BrowserRouter>
           <TopNav {...defaultProps} />
         </BrowserRouter>
-      )
-    })
+      );
+    });
 
     afterEach(() => {
-      component.unmount()
-    })
+      component.unmount();
+    });
 
     it('renders the top-level item', () => {
-      const item = screen.getByRole('link', { name: labelGitHub })
-      expect(item).toBeInTheDocument()
-      expect(item.href).toBe(githubUrl)
-    })
+      const item = screen.getByRole('link', { name: labelGitHub });
+      expect(item).toBeInTheDocument();
+      expect(item.href).toBe(githubUrl);
+    });
 
     it('renders the nested menu items', () => {
-      const item = screen.getAllByText(labelAbout)[0]
-      expect(item).toBeInTheDocument()
-    })
+      const item = screen.getAllByText(labelAbout)[0];
+      expect(item).toBeInTheDocument();
+    });
 
     it('adds target=_self to top-level item', () => {
-      const item = screen.getByRole('link', { name: labelGitHub })
-      expect(item.target).toBe('_self')
-    })
+      const item = screen.getByRole('link', { name: labelGitHub });
+      expect(item.target).toBe('_self');
+    });
 
     it('sets target=_blank by default', () => {
-      const item = screen.getByRole('link', { name: 'Blog' })
-      expect(item.target).toBe('_blank')
-    })
+      const item = screen.getByRole('link', { name: 'Blog' });
+      expect(item.target).toBe('_blank');
+    });
 
     describe('<CustomNavDropdown>', () => {
-      let component
+      let component;
       beforeEach(() => {
-        component = render(<TopNav.CustomNavDropdown {...configMenuGroup} />)
-      })
+        component = render(<TopNav.CustomNavDropdown {...configMenuGroup} />);
+      });
 
       afterEach(() => {
-        component.unmount()
-      })
+        component.unmount();
+      });
 
       it('renders sub-menu text', () => {
         dropdownItems.slice(0, 0).forEach(itemConfig => {
-          const item = getByText(itemConfig.label)
-          expect(item).toBeInTheDocument()
-          expect(item.disabled).toBe(true)
-        })
-      })
+          const item = screen.getByText(itemConfig.label);
+          expect(item).toBeInTheDocument();
+          expect(item.disabled).toBe(true);
+        });
+      });
 
       it('renders sub-menu links', () => {
         dropdownItems.slice(1, 2).forEach(itemConfig => {
           //fireEvent.mouseEnter(screen.getByText(labelAbout))
-          console.log(itemConfig.label)
-          const item = screen.getByRole('link', { name: itemConfig.label })
-          expect(item).toBeInTheDocument()
-          expect(item.href).toBe(itemConfig.url)
-          expect(item.target).toBe(itemConfig.anchorTarget || '_blank')
-        })
-      })
-    })
-  })
-})
+          console.log(itemConfig.label);
+          const item = screen.getByRole('link', { name: itemConfig.label });
+          expect(item).toBeInTheDocument();
+          expect(item.href).toBe(itemConfig.url);
+          expect(item.target).toBe(itemConfig.anchorTarget || '_blank');
+        });
+      });
+    });
+  });
+});
 
 describe('mapStateToProps', () => {
   it('returns entire state', () => {
-    const testState = {}
-    expect(mapStateToProps(testState)).toBe(testState)
-  })
-})
+    const testState = {};
+    expect(mapStateToProps(testState)).toBe(testState);
+  });
+});
