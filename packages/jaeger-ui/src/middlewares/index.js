68c2374d4570f440be1a852185457bdbd20cdf5a
Ansh Goyal <anshgoyal1704@gmail.com>
2023-09-26 15:51:08 +0530

    replace react-router-redux
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- packages/jaeger-ui/src/middlewares/index.js
+++ packages/jaeger-ui/src/middlewares/index.js
@@ -14,7 +14,7 @@
 
 import promiseMiddleware from 'redux-promise-middleware';
 import { change } from 'redux-form';
-import { replace } from 'react-router-redux';
+import { replace } from 'redux-first-history';
 
 import { searchTraces, fetchServiceOperations } from '../actions/jaeger-api';
 import { getUrl as getSearchUrl } from '../components/SearchTracePage/url';
