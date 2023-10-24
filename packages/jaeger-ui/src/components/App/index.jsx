0283dd2788c8c252e01a4eb5c20920e7fba2874a
Ansh Goyal <anshgoyal1704@gmail.com>
2023-10-24 10:52:22 +0530

    replace less variables
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- packages/jaeger-ui/src/components/App/index.jsx
+++ packages/jaeger-ui/src/components/App/index.jsx
@@ -16,6 +16,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Router } from 'react-router-dom';
 
+import { ConfigProvider } from 'antd';
+import { defaultTheme } from '@ant-design/compatible';
 import NotFound from './NotFound';
 import Page from './Page';
 import DependencyGraph from '../DependencyGraph';
@@ -38,6 +40,7 @@ import prefixUrl from '../../utils/prefix-url';
 
 import '../common/vars.css';
 import '../common/utils.css';
+import 'antd/dist/reset.css';
 import './index.css';
 import { history, store } from '../../utils/configure-store';
 import { HistoryProvider } from '../../utils/useHistory';
@@ -51,51 +54,83 @@ export default class JaegerUIApp extends Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <HistoryProvider history={history}>
-          <Router history={history}>
-            <Page>
-              <Switch>
-                <Route path={searchPath}>
-                  <SearchTracePage />
-                </Route>
-                <Route path={traceDiffPath}>
-                  <TraceDiff />
-                </Route>
-                <Route path={tracePath}>
-                  <TracePage />
-                </Route>
-                <Route path={dependenciesPath}>
-                  <DependencyGraph />
-                </Route>
-                <Route path={deepDependenciesPath}>
-                  <DeepDependencies />
-                </Route>
-                <Route path={qualityMetricsPath}>
-                  <QualityMetrics />
-                </Route>
-                <Route path={monitorATMPath}>
-                  <MonitorATMPage />
-                </Route>
+      <ConfigProvider theme={{
+        token: {
+          ...defaultTheme.token,
+          colorPrimary: "#199",
+        },
+        components: {
+          ...defaultTheme.components,
+          Layout: {
+            ...defaultTheme.components.Layout,
+            bodyBg: '#fff',
+            headerBg: '#404040',
+            footerBg: '#fff',
+            headerHeight: 48,
+            headerPadding: '0 50',
+            footerPadding: '24 50',
+            siderBg: '#404040',
+            triggerHeight: 48,
+            triggerBg: 'tint(#fff, 20%)',
+            zeroTriggerWidth: 36,
+            zeroTriggerHeight: 42,
+          },
+          Menu: {
+            ...defaultTheme.components.Menu,
+            darkItemBg: '#151515',
+          },
+          Table: {
+            ...defaultTheme.components.Table,
+            rowHoverBg: '#e5f2f2',
+          }
+        }
+      }}>
+        <Provider store={store}>
+          <HistoryProvider history={history}>
+            <Router history={history}>
+              <Page>
+                <Switch>
+                  <Route path={searchPath}>
+                    <SearchTracePage />
+                  </Route>
+                  <Route path={traceDiffPath}>
+                    <TraceDiff />
+                  </Route>
+                  <Route path={tracePath}>
+                    <TracePage />
+                  </Route>
+                  <Route path={dependenciesPath}>
+                    <DependencyGraph />
+                  </Route>
+                  <Route path={deepDependenciesPath}>
+                    <DeepDependencies />
+                  </Route>
+                  <Route path={qualityMetricsPath}>
+                    <QualityMetrics />
+                  </Route>
+                  <Route path={monitorATMPath}>
+                    <MonitorATMPage />
+                  </Route>
 
-                <Route exact path="/">
-                  <Redirect to={searchPath} />
-                </Route>
-                <Route exact path={prefixUrl()}>
-                  <Redirect to={searchPath} />
-                </Route>
-                <Route exact path={prefixUrl('/')}>
-                  <Redirect to={searchPath} />
-                </Route>
+                  <Route exact path="/">
+                    <Redirect to={searchPath} />
+                  </Route>
+                  <Route exact path={prefixUrl()}>
+                    <Redirect to={searchPath} />
+                  </Route>
+                  <Route exact path={prefixUrl('/')}>
+                    <Redirect to={searchPath} />
+                  </Route>
 
-                <Route>
-                  <NotFound />
-                </Route>
-              </Switch>
-            </Page>
-          </Router>
-        </HistoryProvider>
-      </Provider>
+                  <Route>
+                    <NotFound />
+                  </Route>
+                </Switch>
+              </Page>
+            </Router>
+          </HistoryProvider>
+        </Provider>
+      </ConfigProvider>
     );
   }
 }
