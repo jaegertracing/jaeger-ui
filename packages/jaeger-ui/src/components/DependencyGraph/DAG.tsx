2947a106998f78a67989e1174f5fbf1c57b5ed71
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-12-18 07:20:49 +0530

    Refactoring: DAG migrated to Plexus's Digraph (#1981)
    ## Which problem is this PR solving?
    - This issue is related to LFX task #1466 
    - I can create a more specific sub-issue if that's more suitable
    
    ## Description of the changes
    - I've removed `cytoscape` and replaced it with `Digraph` from `Plexus`
    - Had to add support for labels in `Digraph` to support this change
    
    ## How was this change tested?
    - The changes were tested visually
    - Tested with test data involving around 50 nodes
    
    Before - 
    
    <img width="1785" alt="Screenshot 2023-11-18 at 22 06 10"
    src="https://github.com/jaegertracing/jaeger-ui/assets/28570857/dd130e06-94d5-41b8-8217-7e0cdb442dc2">
    
    Now -
    
    <img width="1785" alt="Screenshot 2023-11-18 at 22 06 22"
    src="https://github.com/jaegertracing/jaeger-ui/assets/28570857/6249207d-b1e6-4774-82ad-2f9e6a589f3e">
    
    Tha additional tab visible in above images has been removed
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [ ] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `yarn lint` and `yarn test`
    
    ---------
    
    Signed-off-by: Prathamesh Mutkure <pmutkure009@gmail.com>
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    Co-authored-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>
    Co-authored-by: Yuri Shkuro <github@ysh.us>

---
+++ packages/jaeger-ui/src/components/DependencyGraph/DAG.tsx
@@ -0,0 +1,139 @@
+// Copyright (c) 2023 The Jaeger Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { ReactNode } from 'react';
+
+import { Digraph, LayoutManager } from '@jaegertracing/plexus';
+import { TEdge, TVertex } from '@jaegertracing/plexus/lib/types';
+
+import './dag.css';
+
+type TServiceCall = {
+  parent: string;
+  child: string;
+  callCount: number;
+};
+
+type TProps = {
+  serviceCalls: TServiceCall[];
+};
+
+export const renderNode = (vertex: TVertex): ReactNode => {
+  return (
+    <div className="DAG--node">
+      <div className="DAG--nodeCircle" />
+      <div className="DAG--nodeLabel" data-testid="dagNodeLabel">
+        {vertex?.key ?? ''}
+      </div>
+    </div>
+  ) as ReactNode;
+};
+
+const formatServiceCalls = (
+  serviceCalls: TServiceCall[]
+): {
+  nodes: TVertex[];
+  edges: TEdge[];
+} => {
+  const nodeMap: Record<string, boolean> = {};
+
+  const nodes: TVertex[] = [];
+  const edges: TEdge[] = [];
+
+  serviceCalls.forEach(d => {
+    if (d.parent.trim().length !== 0 && d.child.trim().length !== 0) {
+      if (!nodeMap[d.parent]) {
+        nodes.push({ key: d.parent });
+        nodeMap[d.parent] = true;
+      }
+
+      if (!nodeMap[d.child]) {
+        nodes.push({ key: d.child });
+        nodeMap[d.child] = true;
+      }
+
+      edges.push({
+        from: d.parent,
+        to: d.child,
+        label: `${d.callCount}`,
+      });
+    }
+  });
+
+  return { nodes, edges };
+};
+
+const { classNameIsSmall } = Digraph.propsFactories;
+
+export default class DAG extends React.Component<TProps> {
+  static defaultProps = {
+    serviceCalls: [],
+  };
+
+  private data: {
+    nodes: TVertex[];
+    edges: TEdge[];
+  };
+
+  private layoutManager: LayoutManager = new LayoutManager({
+    nodesep: 1.5,
+    ranksep: 1.6,
+    rankdir: 'TB',
+    splines: 'polyline',
+    useDotEdges: true,
+  });
+
+  constructor(props: TProps) {
+    super(props);
+
+    this.data = formatServiceCalls(props.serviceCalls);
+  }
+
+  componentWillUnmount() {
+    this.layoutManager.stopAndRelease();
+  }
+
+  render() {
+    return (
+      <div className="DAG">
+        <Digraph<TVertex>
+          zoom
+          minimap
+          className="DAG--dag"
+          setOnGraph={classNameIsSmall}
+          minimapClassName="u-miniMap"
+          layoutManager={this.layoutManager}
+          measurableNodesKey="nodes"
+          layers={[
+            {
+              key: 'edges',
+              defs: [{ localId: 'arrowHead' }],
+              edges: true,
+              layerType: 'svg',
+              markerEndId: 'arrowHead',
+            },
+            {
+              key: 'nodes',
+              layerType: 'html',
+              measurable: true,
+              renderNode,
+            },
+          ]}
+          edges={this.data.edges}
+          vertices={this.data.nodes}
+        />
+      </div>
+    );
+  }
+}
