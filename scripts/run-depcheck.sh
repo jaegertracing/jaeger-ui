de83add157b9ed92967bac9e0c375113c846af34
Yuri Shkuro <github@ysh.us>
2024-01-23 19:04:55 -0500

    Remove unused html-loader
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- scripts/run-depcheck.sh
+++ scripts/run-depcheck.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -ex
 
+tempdir=$(mktemp -d /tmp/depcheckrc.XXXXXX)
+
 # Create a temporary depcheckrc file for 'jaeger-ui'
-tempfile_jaeger=$(mktemp /tmp/depcheckrc.XXXXXX.json)
+tempfile_jaeger="${tempdir}/DepcheckrcJaegerUI.json"
 node scripts/generateDepcheckrcJaegerUI.js "$tempfile_jaeger"
 depcheck packages/jaeger-ui --config "$tempfile_jaeger"
 
 # Create a temporary depcheckrc file for 'plexus'
-tempfile_plexus=$(mktemp /tmp/depcheckrc.XXXXXX.json)
+tempfile_jaeger="${tempdir}/DepcheckrcPlexus.json"
 node scripts/generateDepcheckrcPlexus.js "$tempfile_plexus"
 depcheck packages/plexus --config "$tempfile_plexus"
