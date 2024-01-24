56e0c2316c1c4bac7e80bc9985ac74677f0c8a12
Yuri Shkuro <github@ysh.us>
2024-01-23 19:11:48 -0500

    cleanup
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- scripts/run-depcheck.sh
+++ scripts/run-depcheck.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -ex
 
-tempdir=$(mktemp -d /tmp/depcheckrc.XXXXXX)
+tempdir="$(mktemp -d /tmp/depcheckrc.XXXXXX)"
+cleanup_tempdir() {
+  rm -rf "${tempdir}"
+}
+trap 'cleanup_tempdir' EXIT
 
 # Create a temporary depcheckrc file for 'jaeger-ui'
 tempfile_jaeger="${tempdir}/DepcheckrcJaegerUI.json"
-node scripts/generateDepcheckrcJaegerUI.js "$tempfile_jaeger"
-depcheck packages/jaeger-ui --config "$tempfile_jaeger"
+node scripts/generateDepcheckrcJaegerUI.js "${tempfile_jaeger}"
+depcheck packages/jaeger-ui --config "${tempfile_jaeger}"
 
 # Create a temporary depcheckrc file for 'plexus'
-tempfile_jaeger="${tempdir}/DepcheckrcPlexus.json"
-node scripts/generateDepcheckrcPlexus.js "$tempfile_plexus"
-depcheck packages/plexus --config "$tempfile_plexus"
+tempfile_plexus="${tempdir}/DepcheckrcPlexus.json"
+node scripts/generateDepcheckrcPlexus.js "${tempfile_plexus}"
+depcheck packages/plexus --config "${tempfile_plexus}"
