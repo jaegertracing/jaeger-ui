115075566b2f6bf4094d3bbd0d9511366d064dba
bupd <bupdprasanth@gmail.com>
2024-03-21 00:12:41 +0530

    update: TracePage tooltip arrow convention
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- packages/jaeger-ui/src/components/DeepDependencies/Header/LayoutSettings/index.tsx
+++ packages/jaeger-ui/src/components/DeepDependencies/Header/LayoutSettings/index.tsx
@@ -158,7 +158,12 @@ export default class LayoutSettings extends React.PureComponent<TProps> {
       </table>
     );
     return (
-      <Popover arrow={{ pointAtCenter: true }} content={content} placement="bottomLeft" title="Layout settings">
+      <Popover
+        arrow={{ pointAtCenter: true }}
+        content={content}
+        placement="bottomLeft"
+        title="Layout settings"
+      >
         <div className={cssCls()}>
           {settingsIcon}
           Layout
