754b4ca89a06e30db7b38548b7ebec519d2ffb7c
Hariom Gupta <guptahariom03082003@gmail.com>
2025-01-06 10:42:43 +0530

    change digraph to directedgraph component
    Signed-off-by: Hariom Gupta <guptahariom03082003@gmail.com>

--- packages/plexus/demo/src/index.tsx
+++ packages/plexus/demo/src/index.tsx
@@ -17,6 +17,8 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import largeDag, { getNodeLabel as getLargeNodeLabel, TLargeNode } from './data-large';
+import { edges as dagEdges, vertices as dagVertices } from './data-dag';
+import { colored as colorData, getColorNodeLabel, setOnColorEdge, setOnColorNode } from './data-small';
 import { Digraph, LayoutManager } from '../../src';
 import cacheAs from '../../src/cacheAs';
 import { TLayer, TRendererUtils, TMeasureNodeUtils } from '../../src/Digraph/types';
@@ -33,7 +35,6 @@ const { classNameIsSmall: layeredClassNameIsSmall, scaleStrokeOpacity } = Digrap
 
 const VOWELS = new Set(['a', 'e', 'i', 'o', 'u', 'y']);
 
-const addAnAttr = () => ({ 'data-rando': Math.random() });
 const setOnNode = (vertex: TVertex) => ({
   className: 'DemoGraph--node',
   // eslint-disable-next-line no-console
@@ -185,6 +186,11 @@ class Demo extends React.PureComponent<{}, TState> {
     }
     return null;
   };
+
+  private setOnNode = (vertex: TVertex) => ({
+    className: 'DemoGraph--node',
+    onClick: () => console.log(vertex.key),
+  });
 
   private setUxOnEdge = (layoutEdge: TLayoutEdge<any>) => ({
     onMouseOver: () => this.onEdgeEnter(layoutEdge),
@@ -443,6 +449,173 @@ class Demo extends React.PureComponent<{}, TState> {
               }))}
             />
           </div>
+        </div>
+        <h1>Digraph with cycles - dot edges</h1>
+        <div>
+          <div className="DemoGraph">
+            <Digraph
+              zoom
+              minimap
+              className="DemoGraph--dag"
+              layoutManager={new LayoutManager({ useDotEdges: true })}
+              minimapClassName="Demo--miniMap"
+              measurableNodesKey="nodes"
+              layers={[
+                {
+                  key: 'edges',
+                  defs: [{ localId: 'arrowHead' }],
+                  edges: true,
+                  layerType: 'svg',
+                  markerEndId: 'arrowHead',
+                  setOnContainer: cacheAs('edges/set-on-container', [
+                    { className: 'DdgGraph--edges' },
+                    scaleStrokeOpacity,
+                  ]),
+                },
+                {
+                  key: 'nodes',
+                  layerType: 'html',
+                  measurable: true,
+                  setOnNode: this.setOnNode,
+                  renderNode: getLargeNodeLabel,
+                },
+              ]}
+              {...largeDag}
+            />
+          </div>
+        </div>
+        <h1>Digraph with cycles - neato edges</h1>
+        <div>
+          <div className="DemoGraph">
+            <Digraph
+              zoom
+              minimap
+              className="DemoGraph--dag"
+              layoutManager={new LayoutManager({ useDotEdges: false })}
+              minimapClassName="Demo--miniMap"
+              measurableNodesKey="nodes"
+              layers={[
+                {
+                  key: 'edges',
+                  defs: [{ localId: 'arrowHead' }],
+                  edges: true,
+                  layerType: 'svg',
+                  markerEndId: 'arrowHead',
+                  setOnContainer: cacheAs('neato-edges/set-on-container', [
+                    { className: 'DdgGraph--edges' },
+                    scaleStrokeOpacity,
+                  ]),
+                },
+                {
+                  key: 'nodes',
+                  layerType: 'html',
+                  measurable: true,
+                  setOnNode: this.setOnNode,
+                  renderNode: getLargeNodeLabel,
+                },
+              ]}
+              {...largeDag}
+            />
+          </div>
+        </div>
+        <h1>Digraph with cycles - dot edges - polylines</h1>
+        <div>
+          <div className="DemoGraph">
+            <Digraph
+              zoom
+              minimap
+              className="DemoGraph--dag"
+              layoutManager={
+                new LayoutManager({
+                  useDotEdges: true,
+                  splines: 'polyline',
+                  ranksep: 8,
+                })
+              }
+              minimapClassName="Demo--miniMap"
+              measurableNodesKey="nodes"
+              layers={[
+                {
+                  key: 'edges',
+                  defs: [{ localId: 'arrowHead' }],
+                  edges: true,
+                  layerType: 'svg',
+                  markerEndId: 'arrowHead',
+                  setOnContainer: cacheAs('polyline-edges/set-on-container', [
+                    { className: 'DdgGraph--edges' },
+                    scaleStrokeOpacity,
+                  ]),
+                },
+                {
+                  key: 'nodes',
+                  layerType: 'html',
+                  measurable: true,
+                  setOnNode: this.setOnNode,
+                  renderNode: getLargeNodeLabel,
+                },
+              ]}
+              {...largeDag}
+            />
+          </div>
+        </div>
+        <h1>Small graph with data driven rendering</h1>
+        <div className="DemoGraph">
+          <Digraph
+            className="DemoGraph--dag"
+            zoom
+            minimap
+            layoutManager={new LayoutManager()}
+            measurableNodesKey="nodes"
+            layers={[
+              {
+                key: 'edges',
+                defs: [{ localId: 'arrowHead' }],
+                edges: true,
+                layerType: 'svg',
+                markerEndId: 'arrowHead',
+                setOnContainer: scaleStrokeOpacity,
+                setOnEdge: setOnColorEdge,
+              },
+              {
+                key: 'nodes',
+                layerType: 'html',
+                measurable: true,
+                setOnNode: setOnColorNode,
+                renderNode: getColorNodeLabel,
+              },
+            ]}
+            {...colorData}
+          />
+        </div>
+        <h1>Medium DAG</h1>
+        <div className="DemoGraph">
+          <Digraph
+            zoom
+            minimap
+            className="DemoGraph--dag"
+            layoutManager={new LayoutManager({ useDotEdges: true })}
+            minimapClassName="Demo--miniMap"
+            measurableNodesKey="nodes"
+            layers={[
+              {
+                key: 'edges',
+                defs: [{ localId: 'arrowHead' }],
+                edges: true,
+                layerType: 'svg',
+                markerEndId: 'arrowHead',
+                setOnContainer: scaleStrokeOpacity,
+              },
+              {
+                key: 'nodes',
+                layerType: 'html',
+                measurable: true,
+                setOnNode: this.setOnNode,
+                renderNode: (vertex: TVertex) => vertex.key,
+              },
+            ]}
+            edges={dagEdges}
+            vertices={dagVertices}
+          />
         </div>
       </div>
     );
