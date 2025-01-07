d073538f28f4b2e45f3f1151b42bdd5d1918fe27
YASH PANDEY <pandeyyash7052@gmail.com>
2025-01-08 00:56:23 +0530

    [Deps]: Upgraded react-router-dom
    Signed-off-by: YASH PANDEY <pandeyyash7052@gmail.com>

--- packages/jaeger-ui/src/components/App/index.jsx
+++ packages/jaeger-ui/src/components/App/index.jsx
@@ -11,39 +11,29 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Route, Redirect, Switch, Router } from 'react-router-dom';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import { defaultTheme } from '@ant-design/compatible';
 import NotFound from './NotFound';
 import Page from './Page';
 import DependencyGraph from '../DependencyGraph';
-import { ROUTE_PATH as dependenciesPath } from '../DependencyGraph/url';
 import DeepDependencies from '../DeepDependencies';
-import { ROUTE_PATH as deepDependenciesPath } from '../DeepDependencies/url';
 import QualityMetrics from '../QualityMetrics';
-import { ROUTE_PATH as qualityMetricsPath } from '../QualityMetrics/url';
 import SearchTracePage from '../SearchTracePage';
-import { ROUTE_PATH as searchPath } from '../SearchTracePage/url';
 import TraceDiff from '../TraceDiff';
-import { ROUTE_PATH as traceDiffPath } from '../TraceDiff/url';
 import TracePage from '../TracePage';
-import { ROUTE_PATH as tracePath } from '../TracePage/url';
 import MonitorATMPage from '../Monitor';
-import { ROUTE_PATH as monitorATMPath } from '../Monitor/url';
 import JaegerAPI, { DEFAULT_API_ROOT } from '../../api/jaeger';
 import processScripts from '../../utils/config/process-scripts';
 import prefixUrl from '../../utils/prefix-url';
+import { store } from '../../utils/configure-store';
 
 import '../common/vars.css';
 import '../common/utils.css';
 import 'antd/dist/reset.css';
 import './index.css';
-import { history, store } from '../../utils/configure-store';
-import { HistoryProvider } from '../../utils/useHistory';
 
 const jaegerTheme = {
   token: {
@@ -88,49 +78,21 @@ export default class JaegerUIApp extends Component {
     return (
       <ConfigProvider theme={jaegerTheme}>
         <Provider store={store}>
-          <HistoryProvider history={history}>
-            <Router history={history}>
-              <Page>
-                <Switch>
-                  <Route path={searchPath}>
-                    <SearchTracePage />
-                  </Route>
-                  <Route path={traceDiffPath}>
-                    <TraceDiff />
-                  </Route>
-                  <Route path={tracePath}>
-                    <TracePage />
-                  </Route>
-                  <Route path={dependenciesPath}>
-                    <DependencyGraph />
-                  </Route>
-                  <Route path={deepDependenciesPath}>
-                    <DeepDependencies />
-                  </Route>
-                  <Route path={qualityMetricsPath}>
-                    <QualityMetrics />
-                  </Route>
-                  <Route path={monitorATMPath}>
-                    <MonitorATMPage />
-                  </Route>
-
-                  <Route exact path="/">
-                    <Redirect to={searchPath} />
-                  </Route>
-                  <Route exact path={prefixUrl()}>
-                    <Redirect to={searchPath} />
-                  </Route>
-                  <Route exact path={prefixUrl('/')}>
-                    <Redirect to={searchPath} />
-                  </Route>
-
-                  <Route>
-                    <NotFound />
-                  </Route>
-                </Switch>
-              </Page>
-            </Router>
-          </HistoryProvider>
+            <Page>
+              <Routes>
+                <Route path="/search" element={<SearchTracePage />} />
+                <Route path="/tracediff" element={<TraceDiff />} />
+                <Route path="/trace" element={<TracePage />} />
+                <Route path="/dependencies" element={<DependencyGraph />} />
+                <Route path="/deep-dependencies" element={<DeepDependencies />} />
+                <Route path="/quality-metrics" element={<QualityMetrics />} />
+                <Route path="/monitor-atm" element={<MonitorATMPage />} />
+                <Route path="/" element={<Navigate to="/search" replace />} />
+                <Route path={prefixUrl()} element={<Navigate to="/search" replace />} />
+                <Route path={prefixUrl('/')} element={<Navigate to="/search" replace />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Page>
         </Provider>
       </ConfigProvider>
     );
