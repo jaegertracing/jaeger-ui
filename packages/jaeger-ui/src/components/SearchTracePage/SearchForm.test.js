3621a1f6a3b767d1fbf804a497ca556fee7a2252
Hariom Gupta <guptahariom03082003@gmail.com>
2025-01-17 15:09:40 +0530

    add more tests
    Signed-off-by: Hariom Gupta <guptahariom03082003@gmail.com>

--- packages/jaeger-ui/src/components/SearchTracePage/SearchForm.test.js
+++ packages/jaeger-ui/src/components/SearchTracePage/SearchForm.test.js
@@ -20,6 +20,8 @@ import { shallow } from 'enzyme';
 import dayjs from 'dayjs';
 import queryString from 'query-string';
 import store from 'store';
+import sinon from 'sinon';
+import * as jaegerApiActions from '../../actions/jaeger-api';
 
 import {
   convertQueryParamsToFormDates,
@@ -36,6 +38,7 @@ import {
 } from './SearchForm';
 import * as markers from './SearchForm.markers';
 import getConfig from '../../utils/config/get-config';
+import { FORM_CHANGE_ACTION_TYPE } from '../../constants/search-form';
 
 function makeDateParams(dateOffset = 0) {
   const date = new Date();
@@ -456,9 +459,104 @@ describe('<SearchForm>', () => {
       },
     };
     window.getJaegerUiConfig = jest.fn(() => config);
-    wrapper = shallow(<SearchForm {...defaultProps} selectedService="svcA" />);
+    wrapper = shallow(<SearchForm {...defaultProps} />);
+    wrapper.instance().handleChange({ service: 'svcA' });
     const field = wrapper.find(`Input[name="resultsLimit"]`);
     expect(field.prop('max')).toEqual(maxLimit);
+  });
+
+  it('updates state when tags input changes', () => {
+    const tagsInput = wrapper.find('Input[name="tags"]');
+    tagsInput.simulate('change', { target: { value: 'new=tag' } });
+    expect(wrapper.state('formData').tags).toBe('new=tag');
+  });
+
+  it('updates state when service input changes', () => {
+    const serviceInput = wrapper.find('SearchableSelect[name="service"]');
+    serviceInput.simulate('change', 'svcA');
+    expect(wrapper.state('formData').service).toBe('svcA');
+  });
+
+  it('updates state when operation input changes', () => {
+    const operationInput = wrapper.find('SearchableSelect[name="operation"]');
+    operationInput.simulate('change', 'A');
+    expect(wrapper.state('formData').operation).toBe('A');
+  });
+
+  it('updates state when lookback input changes', () => {
+    const lookbackInput = wrapper.find('SearchableSelect[name="lookback"]');
+    lookbackInput.simulate('change', 'new-lookback');
+    expect(wrapper.state('formData').lookback).toBe('new-lookback');
+  });
+
+  it('updates state when date and time fields input changes', () => {
+    const lookbackInput = wrapper.find('SearchableSelect[name="lookback"]');
+    lookbackInput.simulate('change', 'custom');
+    expect(wrapper.state('formData').lookback).toBe('custom');
+
+    const startDateInput = wrapper.find('Input[name="startDate"]');
+    startDateInput.simulate('change', { target: { value: 'new-date' } });
+    expect(wrapper.state('formData').startDate).toBe('new-date');
+
+    const startDateTimeInput = wrapper.find('Input[name="startDateTime"]');
+    startDateTimeInput.simulate('change', { target: { value: 'new-time' } });
+    expect(wrapper.state('formData').startDateTime).toBe('new-time');
+
+    const endDateInput = wrapper.find('Input[name="endDate"]');
+    endDateInput.simulate('change', { target: { value: 'new-date' } });
+    expect(wrapper.state('formData').endDate).toBe('new-date');
+
+    const endDateTimeInput = wrapper.find('Input[name="endDateTime"]');
+    endDateTimeInput.simulate('change', { target: { value: 'new-time' } });
+    expect(wrapper.state('formData').endDateTime).toBe('new-time');
+  });
+
+  it('updates state when minDuration input changes', () => {
+    const minDurationInput = wrapper.find('ValidatedFormField[name="minDuration"]');
+    minDurationInput.simulate('change', { target: { value: 'new-minDuration' } });
+    expect(wrapper.state('formData').minDuration).toBe('new-minDuration');
+  });
+
+  it('updates state when maxDuration input changes', () => {
+    const maxDurationInput = wrapper.find('ValidatedFormField[name="maxDuration"]');
+    maxDurationInput.simulate('change', { target: { value: 'new-maxDuration' } });
+    expect(wrapper.state('formData').maxDuration).toBe('new-maxDuration');
+  });
+
+  it('updates state when resultsLimit input changes', () => {
+    const resultsLimitInput = wrapper.find('Input[name="resultsLimit"]');
+    resultsLimitInput.simulate('change', { target: { value: 'new-resultsLimit' } });
+    expect(wrapper.state('formData').resultsLimit).toBe('new-resultsLimit');
+  });
+});
+
+describe('submitFormHandler', () => {
+  let wrapper;
+  let submitFormHandler;
+  let preventDefault;
+
+  beforeEach(() => {
+    submitFormHandler = sinon.spy();
+    preventDefault = sinon.spy();
+    wrapper = shallow(
+      <SearchForm
+        {...defaultProps}
+        submitFormHandler={submitFormHandler}
+        initialValues={{ service: 'svcA' }}
+      />
+    );
+  });
+
+  it('calls submitFormHandler with formData on form submit', () => {
+    const formData = wrapper.state('formData');
+    wrapper.instance().handleSubmit({ preventDefault });
+    expect(preventDefault.calledOnce).toBe(true);
+    expect(submitFormHandler.calledOnceWith(formData)).toBe(true);
+  });
+
+  it('prevents default form submission behavior', () => {
+    wrapper.instance().handleSubmit({ preventDefault });
+    expect(preventDefault.calledOnce).toBe(true);
   });
 });
 
@@ -594,5 +692,34 @@ describe('mapDispatchToProps()', () => {
       changeServiceHandler: expect.any(Function),
       submitFormHandler: expect.any(Function),
     });
+  });
+
+  it('should dispatch changeServiceHandler correctly', () => {
+    const dispatch = jest.fn();
+    const { changeServiceHandler } = mapDispatchToProps(dispatch);
+    const service = 'test-service';
+
+    changeServiceHandler(service);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: FORM_CHANGE_ACTION_TYPE,
+      payload: service,
+    });
+  });
+
+  it('should dispatch submitFormHandler correctly', () => {
+    const dispatch = jest.fn();
+    const searchTraces = jest.fn();
+    const fields = {
+      lookback: '1ms',
+      operation: 'A',
+      resultsLimit: 20,
+      service: 'svcA',
+    };
+
+    jest.spyOn(jaegerApiActions, 'searchTraces').mockReturnValue(searchTraces);
+    const { submitFormHandler } = mapDispatchToProps(dispatch);
+    submitFormHandler(fields);
+    expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
   });
 });
