8f9393c0202d46978c3942408ab3f74fa6970936
jordanyono <jordanyono@gmail.com>
2025-05-20 23:43:17 -0400

    adding horizontal scroll to searchable select
    Signed-off-by: jordanyono <jordanyono@gmail.com>

--- packages/jaeger-ui/src/components/common/SearchableSelect.tsx
+++ packages/jaeger-ui/src/components/common/SearchableSelect.tsx
@@ -15,13 +15,20 @@
 import React, { FunctionComponent } from 'react';
 import { Select, SelectProps } from 'antd';
 import { DefaultOptionType } from 'antd/es/select';
+import './SearchableSelect.css';
 
 export const filterOptionsByLabel = (input: string, option?: DefaultOptionType) => {
   return (option?.children?.toString() ?? '').toLowerCase().includes(input.toLowerCase());
 };
 
 const SearchableSelect: FunctionComponent<SelectProps> = props => {
-  return <Select showSearch filterOption={filterOptionsByLabel} {...props} />;
+  return (
+    <Select
+      showSearch
+      filterOption={filterOptionsByLabel}
+      {...props}
+    />
+  );
 };
 
 export default SearchableSelect;
