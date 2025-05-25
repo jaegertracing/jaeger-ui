dcda43fa06bb4aaf9ecb0e222ad0f14565f6d4ba
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-05-25 23:07:44 +0530

    format code
    Signed-off-by: Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/TraceStatistics/generateColor.test.js
+++ packages/jaeger-ui/src/components/TracePage/TraceStatistics/generateColor.test.js
@@ -117,16 +117,15 @@ describe('generateColor', () => {
     expect(tableValue2[1].colorToPercent).toBe('rgb(248,248,248)');
   });
 
-it('covers percent attribute with colorToPercent=true', () => {
-  const input = [
-    { isDetail: false, percent: 75 },
-    { isDetail: false, percent: 25 },
-  ];
+  it('covers percent attribute with colorToPercent=true', () => {
+    const input = [
+      { isDetail: false, percent: 75 },
+      { isDetail: false, percent: 25 },
+    ];
 
-  const output = generateColor(input, 'percent', true);
+    const output = generateColor(input, 'percent', true);
 
-  expect(output[0].colorToPercent).toBe('rgb(248,111.5,111.5)');
-  expect(output[1].colorToPercent).toBe('rgb(248,194.5,194.5)');
-});
-
+    expect(output[0].colorToPercent).toBe('rgb(248,111.5,111.5)');
+    expect(output[1].colorToPercent).toBe('rgb(248,194.5,194.5)');
+  });
 });
