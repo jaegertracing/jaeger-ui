f97f481a33470316bff10a70f60fa8fbb51b2ce6
Parship Chowdhury <i.am.parship@gmail.com>
2025-05-30 14:34:25 +0000

    fix codecov coverage
    Signed-off-by: Parship Chowdhury <i.am.parship@gmail.com>

--- packages/jaeger-ui/src/components/SearchTracePage/FileLoader.test.js
+++ packages/jaeger-ui/src/components/SearchTracePage/FileLoader.test.js
@@ -18,11 +18,27 @@ import '@testing-library/jest-dom';
 
 import FileLoader from './FileLoader';
 
+jest.mock('antd', () => ({
+  ...jest.requireActual('antd'),
+  Upload: {
+    ...jest.requireActual('antd').Upload,
+    Dragger: jest.fn(({ beforeUpload, children, ...props }) => {
+      global.mockBeforeUpload = beforeUpload;
+      return (
+        <div data-testid="upload-dragger" {...props}>
+          {children}
+        </div>
+      );
+    }),
+  },
+}));
+
 describe('<FileLoader />', () => {
   const mockLoadJsonTraces = jest.fn();
 
   beforeEach(() => {
     mockLoadJsonTraces.mockClear();
+    global.mockBeforeUpload = null;
   });
 
   it('renders the file upload area', () => {
@@ -39,5 +55,22 @@ describe('<FileLoader />', () => {
     fileList.forEach(fileFromList => mockLoadJsonTraces({ file: fileFromList }));
 
     expect(mockLoadJsonTraces).toHaveBeenCalledWith({ file });
+  });
+
+  it('beforeUpload callback calls loadJsonTraces for each file and returns false', () => {
+    render(<FileLoader loadJsonTraces={mockLoadJsonTraces} />);
+    const beforeUpload = global.mockBeforeUpload;
+    expect(beforeUpload).toBeDefined();
+
+    const file1 = new File(['{"trace": "data1"}'], 'trace1.json', { type: 'application/json' });
+    const file2 = new File(['{"trace": "data2"}'], 'trace2.json', { type: 'application/json' });
+    const fileList = [file1, file2];
+
+    const result = beforeUpload(file1, fileList);
+
+    expect(mockLoadJsonTraces).toHaveBeenCalledTimes(2);
+    expect(mockLoadJsonTraces).toHaveBeenCalledWith({ file: file1 });
+    expect(mockLoadJsonTraces).toHaveBeenCalledWith({ file: file2 });
+    expect(result).toBe(false);
   });
 });
