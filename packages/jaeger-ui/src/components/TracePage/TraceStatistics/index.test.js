bf0242651398a843e1d5745210248cc0c8037065
Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>
2025-07-04 19:29:00 +0530

    increase test coverage
    Signed-off-by: Vishvamsinh Vaghela <vaghelavishvamsinh11111@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/TraceStatistics/index.test.js
+++ packages/jaeger-ui/src/components/TracePage/TraceStatistics/index.test.js
@@ -89,4 +89,209 @@ describe('<TraceTagOverview>', () => {
     expect(instance.state.popupContent).toBe('select *');
     expect(instance.state.showPopup).toBe(false);
   });
+
+  it('should trigger onClickOption when clicking on name cell with sql.query selector', () => {
+    const instance = wrapper.instance();
+    instance.setState({ valueNameSelector1: 'sql.query' });
+    
+    const mockTableData = [
+      {
+        name: 'SELECT * FROM users',
+        hasSubgroupValue: true,
+        searchColor: 'transparent',
+        key: '0'
+      }
+    ];
+    
+    instance.setState({ tableValue: mockTableData });
+    wrapper.update();
+    
+    const nameCell = wrapper.find('[role="button"]').first();
+    nameCell.simulate('click');
+    
+    expect(wrapper.state('showPopup')).toBe(true);
+    expect(wrapper.state('popupContent')).toBe('SELECT * FROM users');
+  });
+
+  it('should handle onClickOption when hasSubgroupValue is false', () => {
+    const instance = wrapper.instance();
+    instance.setState({ valueNameSelector1: 'sql.query' });
+    
+    const mockTableData = [
+      {
+        name: 'test-name',
+        hasSubgroupValue: false,
+        searchColor: 'transparent',
+        key: '0'
+      }
+    ];
+    
+    instance.setState({ tableValue: mockTableData });
+    wrapper.update();
+    
+    const nameCell = wrapper.find('[role="button"]').first();
+    nameCell.simulate('click');
+    
+    expect(wrapper.state('showPopup')).toBe(false);
+  });
+
+  it('should test sorter function with string comparison', () => {
+    const instance = wrapper.instance();
+    const mockTableData = [
+      {
+        name: 'zebra',
+        hasSubgroupValue: true,
+        count: 1,
+        key: '0'
+      },
+      {
+        name: 'alpha',
+        hasSubgroupValue: true,
+        count: 2,
+        key: '1'
+      }
+    ];
+    
+    instance.setState({ tableValue: mockTableData });
+    wrapper.update();
+    
+    const nameColumnHeader = wrapper.find('.ant-table-column-sorters').first();
+    nameColumnHeader.simulate('click');
+    
+    wrapper.update();
+  });
+
+  it('should test sorter function with items that have no hasSubgroupValue', () => {
+    const instance = wrapper.instance();
+    
+    const mockTableData = [
+      {
+        name: 'item1',
+        hasSubgroupValue: false,
+        count: 1,
+        key: '0'
+      },
+      {
+        name: 'item2',
+        hasSubgroupValue: true,
+        count: 2,
+        key: '1'
+      },
+      {
+        name: 'item3',
+        hasSubgroupValue: false,
+        count: 3,
+        key: '2'
+      }
+    ];
+    
+    instance.setState({ tableValue: mockTableData });
+    wrapper.update();
+    
+    const countColumnHeader = wrapper.find('.ant-table-column-sorters').at(1);
+    countColumnHeader.simulate('click');
+    
+    wrapper.update();
+  });
+
+  it('should test searchInTable with complex search scenarios', () => {
+    const instance = wrapper.instance();
+    
+    const mockTableData = [
+      {
+        name: 'parent1',
+        isDetail: false,
+        hasSubgroupValue: true,
+        parentElement: 'none',
+        searchColor: 'rgb(248,248,248)',
+        key: '0'
+      },
+      {
+        name: 'detail1',
+        isDetail: true,
+        hasSubgroupValue: false,
+        parentElement: 'parent1',
+        searchColor: 'rgb(248,248,248)',
+        key: '1'
+      },
+      {
+        name: 'detail2',
+        isDetail: true,
+        hasSubgroupValue: false,
+        parentElement: 'parent2',
+        searchColor: 'rgb(248,248,248)',
+        key: '2'
+      }
+    ];
+    
+    const searchSet = new Set(['parent1detail1']);
+    
+    const result = instance.searchInTable(searchSet, mockTableData, null);
+    
+    expect(result).toBeDefined();
+  });
+
+  it('should test searchInTable with uiFind matching and detail items', () => {
+    const instance = wrapper.instance();
+    
+    const mockTableData = [
+      {
+        name: 'searchterm',
+        isDetail: true,
+        hasSubgroupValue: false,
+        parentElement: 'parentitem',
+        searchColor: 'rgb(248,248,248)',
+        key: '0'
+      },
+      {
+        name: 'parentitem',
+        isDetail: false,
+        hasSubgroupValue: true,
+        parentElement: 'none',
+        searchColor: 'rgb(248,248,248)',
+        key: '1'
+      },
+      {
+        name: 'childitem',
+        isDetail: true,
+        hasSubgroupValue: false,
+        parentElement: 'searchterm',
+        searchColor: 'rgb(248,248,248)',
+        key: '2'
+      }
+    ];
+    
+    const result = instance.searchInTable(undefined, mockTableData, 'searchterm');
+    
+    const highlightedItems = result.filter(item => item.searchColor === 'rgb(255,243,215)');
+    expect(highlightedItems.length).toBeGreaterThan(0);
+  });
+
+  it('should test searchInTable with items that have subgroup values but are details', () => {
+    const instance = wrapper.instance();
+    
+    const mockTableData = [
+      {
+        name: 'item1',
+        isDetail: true,
+        hasSubgroupValue: true,
+        parentElement: 'none',
+        searchColor: undefined,
+        key: '0'
+      },
+      {
+        name: 'item2',
+        isDetail: false,
+        hasSubgroupValue: false,
+        parentElement: 'none',
+        searchColor: undefined,
+        key: '1'
+      }
+    ];
+    
+    const result = instance.searchInTable(undefined, mockTableData, null);
+    
+    expect(result[0].searchColor).toBe('rgb(248,248,248)');
+    expect(result[1].searchColor).toBe('rgb(248,248,248)');
+  });
 });
