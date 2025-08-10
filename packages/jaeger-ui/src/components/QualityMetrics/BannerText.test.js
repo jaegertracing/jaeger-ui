bebdcc53955b8d01cc2f1252ff0658e11e37dfae
Yuri Shkuro <github@ysh.us>
2025-08-09 20:52:56 -0400

    remove trailing slashes in imports
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- packages/jaeger-ui/src/components/QualityMetrics/BannerText.test.js
+++ packages/jaeger-ui/src/components/QualityMetrics/BannerText.test.js
@@ -14,7 +14,7 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/';
+import '@testing-library/jest-dom';
 import BannerText from './BannerText';
 
 describe('BannerText', () => {
