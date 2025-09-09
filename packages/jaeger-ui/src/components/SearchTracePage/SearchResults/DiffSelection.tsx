8b0b079dcf8c0d366de039503d2840f383c0211f
JeevaRamanathan <jeevaramanathan.m@infosys.com>
2025-09-09 16:28:55 +0000

    convert SearchResults/DiffSelection to functional component and improved testcases
    Signed-off-by: JeevaRamanathan <jeevaramanathan.m@infosys.com>

--- packages/jaeger-ui/src/components/SearchTracePage/SearchResults/DiffSelection.tsx
+++ packages/jaeger-ui/src/components/SearchTracePage/SearchResults/DiffSelection.tsx
@@ -33,50 +33,50 @@ type Props = {
 // Exported for tests
 export const CTA_MESSAGE = <h2 className="ub-m0">Compare traces by selecting result items</h2>;
 
-export default class DiffSelection extends React.PureComponent<Props> {
-  render() {
-    const { toggleComparison, traces } = this.props;
-    const cohort = traces.filter(ft => ft.state !== fetchedState.ERROR).map(ft => ft.id);
-    const compareHref = cohort.length > 1 ? getUrl({ cohort }) : null;
-    const compareBtn = (
-      <Button className="ub-right" disabled={cohort.length < 2} htmlType="button" type="primary">
-        Compare Traces
-      </Button>
-    );
-    return (
-      <div className={`DiffSelection ${traces.length ? 'is-non-empty' : ''} ub-mb3`}>
-        {traces.length > 0 && (
-          <div className="DiffSelection--selectedItems">
-            {traces.map(fetchedTrace => {
-              const { data, error, id, state } = fetchedTrace;
-              return (
-                <ResultItemTitle
-                  key={id}
-                  duration={data && data.duration}
-                  error={error}
-                  isInDiffCohort
-                  linkTo={getTracePageUrl(id)}
-                  state={state}
-                  targetBlank
-                  toggleComparison={toggleComparison}
-                  traceID={id}
-                  traceName={data && data.traceName}
-                />
-              );
-            })}
-          </div>
-        )}
-        <div className="DiffSelection--message">
-          {traces.length > 0 ? (
-            <React.Fragment>
-              {compareHref ? <Link to={compareHref}>{compareBtn}</Link> : compareBtn}
-              <h2 className="ub-m0">{cohort.length} Selected for comparison</h2>
-            </React.Fragment>
-          ) : (
-            CTA_MESSAGE
-          )}
+export default function DiffSelection({ toggleComparison, traces }: Props) {
+  const cohort = traces.filter(ft => ft.state !== fetchedState.ERROR).map(ft => ft.id);
+
+  const compareHref = cohort.length > 1 ? getUrl({ cohort }) : null;
+
+  const compareBtn = (
+    <Button className="ub-right" disabled={cohort.length < 2} htmlType="button" type="primary">
+      Compare Traces
+    </Button>
+  );
+
+  return (
+    <div className={`DiffSelection ${traces.length ? 'is-non-empty' : ''} ub-mb3`}>
+      {traces.length > 0 && (
+        <div className="DiffSelection--selectedItems">
+          {traces.map(fetchedTrace => {
+            const { data, error, id, state } = fetchedTrace;
+            return (
+              <ResultItemTitle
+                key={id}
+                duration={data && data.duration}
+                error={error}
+                isInDiffCohort
+                linkTo={getTracePageUrl(id)}
+                state={state}
+                targetBlank
+                toggleComparison={toggleComparison}
+                traceID={id}
+                traceName={data && data.traceName}
+              />
+            );
+          })}
         </div>
+      )}
+      <div className="DiffSelection--message">
+        {traces.length > 0 ? (
+          <React.Fragment>
+            {compareHref ? <Link to={compareHref}>{compareBtn}</Link> : compareBtn}
+            <h2 className="ub-m0">{cohort.length} Selected for comparison</h2>
+          </React.Fragment>
+        ) : (
+          CTA_MESSAGE
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
