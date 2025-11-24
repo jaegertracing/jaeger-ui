9e3d1852669d9e7467dac93f24e0f42663b40bd2
Yuri Shkuro <github@ysh.us>
2025-11-23 20:08:47 -0500

    more
    Signed-off-by: Yuri Shkuro <github@ysh.us>

---
+++ packages/jaeger-ui/src/model/order-by.ts
@@ -0,0 +1,8 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export const MOST_RECENT = 'MOST_RECENT';
+export const LONGEST_FIRST = 'LONGEST_FIRST';
+export const SHORTEST_FIRST = 'SHORTEST_FIRST';
+export const MOST_SPANS = 'MOST_SPANS';
+export const LEAST_SPANS = 'LEAST_SPANS';
