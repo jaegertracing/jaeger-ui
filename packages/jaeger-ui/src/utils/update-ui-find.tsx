ec83e85bba3a89b83ba314d900fa3287887d8bcb
Parship Chowdhury <i.am.parship@gmail.com>
2025-12-11 17:37:39 +0000

    make update-ui-find.tsx backward compatible
    Signed-off-by: Parship Chowdhury <i.am.parship@gmail.com>

--- packages/jaeger-ui/src/utils/update-ui-find.tsx
+++ packages/jaeger-ui/src/utils/update-ui-find.tsx
@@ -2,18 +2,20 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import queryString from 'query-string';
-
+import { NavigateFunction } from 'react-router-dom-v5-compat';
 import { History as RouterHistory, Location } from 'history';
 
 import { TNil } from '../types';
 
 export default function updateUiFind({
   history,
+  navigate,
   location,
   trackFindFunction,
   uiFind,
 }: {
-  history: RouterHistory;
+  history?: RouterHistory;
+  navigate?: NavigateFunction;
   location: Location;
   trackFindFunction?: (uiFind: string | TNil) => void;
   uiFind?: string | TNil;
@@ -21,8 +23,15 @@ export default function updateUiFind({
   const { uiFind: _oldUiFind, ...queryParams } = queryString.parse(location.search);
   if (trackFindFunction) trackFindFunction(uiFind);
   if (uiFind) (queryParams as Record<string, string>).uiFind = uiFind;
-  history.replace({
-    ...location,
-    search: `?${queryString.stringify(queryParams)}`,
-  });
+  if (navigate) {
+    navigate(
+      { pathname: location.pathname, search: `?${queryString.stringify(queryParams)}` },
+      { replace: true }
+    );
+  } else if (history) {
+    history.replace({
+      ...location,
+      search: `?${queryString.stringify(queryParams)}`,
+    });
+  }
 }
