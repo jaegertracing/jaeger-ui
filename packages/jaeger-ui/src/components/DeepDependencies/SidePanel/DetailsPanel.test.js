1457f957f98d82b865ef31bc94df72143170660c
aaryan359 <aaryanmeena96@gmail.com>
2026-01-15 02:19:42 +0530

    Refactor DetailsPanel to functional component
    Signed-off-by: aaryan359 <aaryanmeena96@gmail.com>

--- packages/jaeger-ui/src/components/DeepDependencies/SidePanel/DetailsPanel.test.js
+++ packages/jaeger-ui/src/components/DeepDependencies/SidePanel/DetailsPanel.test.js
@@ -8,7 +8,13 @@ import _set from 'lodash/set';
 
 import stringSupplant from '../../../utils/stringSupplant';
 import JaegerAPI from '../../../api/jaeger';
-import { UnconnectedDetailsPanel as DetailsPanel } from './DetailsPanel';
+import DetailsPanel from './DetailsPanel';
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+}));
+
+import { useSelector } from 'react-redux';
 
 jest.mock('../../common/VerticalResizer', () => {
   return ({ onChange, position }) => (
@@ -31,9 +37,22 @@ jest.mock('../../common/DetailsCard', () => {
   );
 });
 
-describe('<SidePanel>', () => {
+describe('<DetailsPanel />', () => {
   const service = 'test svc';
   const opString = 'test op';
+
+  const baseDecorationState = {
+    decorationValue: 'test decorationValue',
+    decorationProgressbar: null,
+  };
+
+  const setSelectorState = overrides => {
+    useSelector.mockReturnValue({
+      ...baseDecorationState,
+      ...overrides,
+    });
+  };
+
   const props = {
     decorationSchema: {
       detailUrl: 'http://test.detail.url?someParam=#{service}',
@@ -42,14 +61,15 @@ describe('<SidePanel>', () => {
       opDetailPath: 'test.opDetail.path',
       name: 'Decorating #{service}',
     },
-    decorationValue: 'test decorationValue',
     service,
   };
+
   const supplantedUrl = stringSupplant(props.decorationSchema.detailUrl, { service });
   const supplantedOpUrl = stringSupplant(props.decorationSchema.opDetailUrl, {
     operation: opString,
     service,
   });
+
   let fetchDecorationSpy;
   let promise;
   let res;
@@ -66,211 +86,123 @@ describe('<SidePanel>', () => {
   });
 
   beforeEach(() => {
-    fetchDecorationSpy.mockClear();
     jest.clearAllMocks();
+    setSelectorState();
   });
 
-  describe('fetchDetails', () => {
-    it('fetches correct details url, perferring op-scoped details, or does not fetch at all', () => {
-      const component1 = render(<DetailsPanel {...props} operation={opString} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledWith(supplantedOpUrl);
-      component1.unmount();
-      fetchDecorationSpy.mockClear();
+  // ---------------- fetchDetails ----------------
 
-      const component2 = render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledWith(supplantedUrl);
-      component2.unmount();
-      fetchDecorationSpy.mockClear();
+  it('fetches correct details url, preferring op-scoped details, or does not fetch at all', () => {
+    const component1 = render(<DetailsPanel {...props} operation={opString} />);
+    expect(fetchDecorationSpy).toHaveBeenCalledWith(supplantedOpUrl);
+    component1.unmount();
 
-      const component3 = render(
-        <DetailsPanel
-          service={service}
-          decorationSchema={{
-            name: 'Test without URLs',
-          }}
-        />
-      );
-      expect(fetchDecorationSpy).not.toHaveBeenCalled();
-      component3.unmount();
-    });
+    fetchDecorationSpy.mockClear();
+
+    const component2 = render(<DetailsPanel {...props} />);
+    expect(fetchDecorationSpy).toHaveBeenCalledWith(supplantedUrl);
+    component2.unmount();
+
+    fetchDecorationSpy.mockClear();
+
+    const component3 = render(
+      <DetailsPanel
+        service={service}
+        decorationSchema={{
+          name: 'Test without URLs',
+        }}
+      />
+    );
+    expect(fetchDecorationSpy).not.toHaveBeenCalled();
+    component3.unmount();
   });
 
-  describe('render', () => {
-    it('renders', () => {
-      const { container } = render(<DetailsPanel {...props} />);
+  // ---------------- render ----------------
 
-      expect(container.textContent).toContain(service);
-
-      expect(container.textContent).toContain(`Decorating ${service}`);
-
-      expect(container.textContent).toContain(props.decorationValue);
-    });
-
-    it('renders with operation', () => {
-      const { container } = render(<DetailsPanel {...props} operation={opString} />);
-
-      expect(container.textContent).toContain(service);
-      expect(container.textContent).toContain(opString);
-    });
-
-    it('renders omitted array of operations', () => {
-      const { container } = render(<DetailsPanel {...props} operation={['op0', 'op1']} />);
-
-      expect(container.textContent).toContain(service);
-
-      expect(container.textContent).not.toContain('op0');
-      expect(container.textContent).not.toContain('op1');
-    });
-
-    it('renders with progressbar', () => {
-      const progressbarText = 'stand-in progressbar';
-      const progressbar = <div data-testid="test-progressbar">{progressbarText}</div>;
-      const { container } = render(<DetailsPanel {...props} decorationProgressbar={progressbar} />);
-
-      expect(screen.getByTestId('test-progressbar')).toBeInTheDocument();
-      expect(screen.getByText(progressbarText)).toBeInTheDocument();
-
-      expect(container.textContent).not.toContain(props.decorationValue);
-    });
-
-    it('renders while loading', () => {
-      const { container } = render(<DetailsPanel {...props} />);
-      expect(container.querySelector('.Ddg--DetailsPanel--LoadingWrapper')).toBeInTheDocument();
-    });
-
-    it('renders details', async () => {
-      render(<DetailsPanel {...props} />);
-
-      const detailsData = 'details string';
-      const response = {};
-      _set(response, props.decorationSchema.detailPath, detailsData);
-
-      res(response);
-
-      const detailsCard = await screen.findByTestId('details-card');
-      expect(detailsCard).toBeInTheDocument();
-      expect(detailsCard.textContent).toBe(detailsData);
-    });
-
-    it('renders details error', async () => {
-      render(<DetailsPanel {...props} />);
-
-      rej(new Error('fetch error'));
-
-      const detailsCard = await screen.findByTestId('details-card');
-      expect(detailsCard).toBeInTheDocument();
-      expect(detailsCard).toHaveClass('is-error');
-      expect(detailsCard.textContent).toContain('Unable to fetch decoration: fetch error');
-    });
-
-    it('renders detailLink', () => {
-      const schemaWithLink = {
-        ...props.decorationSchema,
-        detailLink: 'test details link',
-      };
-      render(<DetailsPanel {...props} decorationSchema={schemaWithLink} />);
-
-      const link = screen.getByRole('link');
-      expect(link).toHaveAttribute('href', schemaWithLink.detailLink);
-      expect(link).toHaveAttribute('target', '_blank');
-      expect(link).toHaveAttribute('rel', 'noreferrer noopener');
-    });
-
-    it('renders details path not found error', async () => {
-      render(<DetailsPanel {...props} />);
-
-      const response = { someOtherData: 'value' };
-
-      res(response);
-
-      const detailsCard = await screen.findByTestId('details-card');
-      expect(detailsCard).toBeInTheDocument();
-      expect(detailsCard).toHaveClass('is-error');
-      expect(detailsCard.textContent).toContain(
-        `\`${props.decorationSchema.detailPath}\` not found in response`
-      );
-    });
+  it('renders', () => {
+    const { container } = render(<DetailsPanel {...props} />);
+    expect(container).toHaveTextContent(service);
+    expect(container).toHaveTextContent(`Decorating ${service}`);
+    expect(container).toHaveTextContent(baseDecorationState.decorationValue);
   });
 
-  describe('componentDidMount', () => {
-    it('fetches details', () => {
-      expect(fetchDecorationSpy).not.toHaveBeenCalled();
-
-      render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalled();
-      expect(fetchDecorationSpy).toHaveBeenLastCalledWith(supplantedUrl);
-    });
+  it('renders with operation', () => {
+    const { container } = render(<DetailsPanel {...props} operation={opString} />);
+    expect(container).toHaveTextContent(service);
+    expect(container).toHaveTextContent(opString);
   });
 
-  describe('componentDidUpdate', () => {
-    it('fetches details and clears relevant state if decorationSchema changes', async () => {
-      const { rerender } = render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-
-      fetchDecorationSpy.mockClear();
-
-      const detailUrl = 'http://new.schema.detailsUrl?service=#{service}';
-      const newSchema = {
-        ...props.decorationSchema,
-        detailUrl,
-      };
-
-      rerender(<DetailsPanel {...props} decorationSchema={newSchema} />);
-
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-      expect(fetchDecorationSpy).toHaveBeenLastCalledWith(stringSupplant(detailUrl, { service }));
-    });
-
-    it('fetches details and clears relevant state if operation changes', () => {
-      const { rerender } = render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-
-      fetchDecorationSpy.mockClear();
-
-      rerender(<DetailsPanel {...props} operation={opString} />);
-
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-      expect(fetchDecorationSpy).toHaveBeenLastCalledWith(supplantedOpUrl);
-    });
-
-    it('fetches details and clears relevant state if service changes', () => {
-      const { rerender } = render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-
-      fetchDecorationSpy.mockClear();
-
-      const newService = 'different test service';
-      rerender(<DetailsPanel {...props} service={newService} />);
-
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-      expect(fetchDecorationSpy).toHaveBeenLastCalledWith(
-        stringSupplant(props.decorationSchema.detailUrl, { service: newService })
-      );
-    });
-
-    it('does nothing if decorationSchema, operation, and service are unchanged', () => {
-      const { rerender } = render(<DetailsPanel {...props} />);
-      expect(fetchDecorationSpy).toHaveBeenCalledTimes(1);
-
-      fetchDecorationSpy.mockClear();
-
-      rerender(<DetailsPanel {...props} decorationValue={`not_${props.decorationValue}`} />);
-
-      expect(fetchDecorationSpy).not.toHaveBeenCalled();
-    });
+  it('renders omitted array of operations', () => {
+    const { container } = render(<DetailsPanel {...props} operation={['op0', 'op1']} />);
+    expect(container).toHaveTextContent(service);
+    expect(container).not.toHaveTextContent('op0');
+    expect(container).not.toHaveTextContent('op1');
   });
 
-  describe('onResize', () => {
-    it('updates state', () => {
-      const { container } = render(<DetailsPanel {...props} />);
+  it('renders with progressbar', () => {
+    const progressbarText = 'stand-in progressbar';
+    const progressbar = <div data-testid="test-progressbar">{progressbarText}</div>;
 
-      const initialWidth = '30vw';
-      expect(container.querySelector('.Ddg--DetailsPanel')).toHaveStyle(`width: ${initialWidth}`);
-
-      const resizer = screen.getByTestId('vertical-resizer');
-      fireEvent.click(resizer);
-
-      expect(container.querySelector('.Ddg--DetailsPanel')).toHaveStyle('width: 60vw');
+    setSelectorState({
+      decorationProgressbar: progressbar,
+      decorationValue: 'unused',
     });
+
+    render(<DetailsPanel {...props} />);
+    expect(screen.getByTestId('test-progressbar')).toBeInTheDocument();
+  });
+
+  it('renders while loading', () => {
+    render(<DetailsPanel {...props} />);
+    expect(document.querySelector('.Ddg--DetailsPanel--LoadingWrapper')).toBeInTheDocument();
+  });
+
+  it('renders details', async () => {
+    render(<DetailsPanel {...props} />);
+
+    const detailsData = 'details string';
+    const response = {};
+    _set(response, props.decorationSchema.detailPath, detailsData);
+
+    res(response);
+
+    const detailsCard = await screen.findByTestId('details-card');
+    expect(detailsCard.textContent).toBe(detailsData);
+  });
+
+  it('renders details error', async () => {
+    render(<DetailsPanel {...props} />);
+    rej(new Error('fetch error'));
+
+    const detailsCard = await screen.findByTestId('details-card');
+    expect(detailsCard).toHaveClass('is-error');
+  });
+
+  it('renders detailLink', () => {
+    const schemaWithLink = {
+      ...props.decorationSchema,
+      detailLink: 'test details link',
+    };
+
+    render(<DetailsPanel {...props} decorationSchema={schemaWithLink} />);
+    expect(screen.getByRole('link')).toHaveAttribute('href', schemaWithLink.detailLink);
+  });
+
+  it('renders details path not found error', async () => {
+    render(<DetailsPanel {...props} />);
+    res({ someOtherData: 'value' });
+
+    const detailsCard = await screen.findByTestId('details-card');
+    expect(detailsCard).toHaveClass('is-error');
+  });
+
+  // ---------------- onResize ----------------
+
+  it('updates width when resized', () => {
+    const { container } = render(<DetailsPanel {...props} />);
+    expect(container.querySelector('.Ddg--DetailsPanel')).toHaveStyle('width: 30vw');
+
+    fireEvent.click(screen.getByTestId('vertical-resizer'));
+    expect(container.querySelector('.Ddg--DetailsPanel')).toHaveStyle('width: 60vw');
   });
 });
