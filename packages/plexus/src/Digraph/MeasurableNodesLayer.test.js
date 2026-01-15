348d5f1b12c8369a1a6d69e2bf10400d23ffdc09
thc1006 <84045975+thc1006@users.noreply.github.com>
2026-01-15 21:13:31 +0800

    fix: use React.createElement in jest.mock factories
    Replace JSX syntax with React.createElement in jest.mock factory
    functions to avoid "Invalid variable access: _jsx" error in CI.
    
    Signed-off-by: thc1006 <84045975+thc1006@users.noreply.github.com>

--- packages/plexus/src/Digraph/MeasurableNodesLayer.test.js
+++ packages/plexus/src/Digraph/MeasurableNodesLayer.test.js
@@ -11,33 +11,37 @@ const mockMeasurableNodesProps = [];
 const mockHtmlLayerProps = [];
 const mockSvgLayerProps = [];
 
+// 使用 React.createElement 而非 JSX，因為 jest.mock 的工廠函數不允許引用外部變數
 jest.mock('./MeasurableNodes', () => {
+  const React = require('react');
   const MockMeasurableNodes = props => {
     mockMeasurableNodesProps.push(props);
-    return <g data-testid="measurable-nodes" />;
+    return React.createElement('g', { 'data-testid': 'measurable-nodes' });
   };
   return MockMeasurableNodes;
 });
 
 jest.mock('./HtmlLayer', () => {
+  const React = require('react');
   const MockHtmlLayer = ({ children, classNamePart, ...rest }) => {
     mockHtmlLayerProps.push({ classNamePart, ...rest });
-    return (
-      <div data-testid="html-layer" data-classname-part={classNamePart}>
-        {children}
-      </div>
+    return React.createElement(
+      'div',
+      { 'data-testid': 'html-layer', 'data-classname-part': classNamePart },
+      children
     );
   };
   return MockHtmlLayer;
 });
 
 jest.mock('./SvgLayer', () => {
+  const React = require('react');
   const MockSvgLayer = ({ children, classNamePart, ...rest }) => {
     mockSvgLayerProps.push({ classNamePart, ...rest });
-    return (
-      <svg data-testid="svg-layer" data-classname-part={classNamePart}>
-        {children}
-      </svg>
+    return React.createElement(
+      'svg',
+      { 'data-testid': 'svg-layer', 'data-classname-part': classNamePart },
+      children
     );
   };
   return MockSvgLayer;
