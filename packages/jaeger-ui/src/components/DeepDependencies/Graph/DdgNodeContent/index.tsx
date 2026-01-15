7bb20275b04a361d95a9b74c255052fa0cecca35
insane-22 <bhumikabachchan11315@gmail.com>
2026-01-15 18:07:50 +0000

    fix: fixed suggestions
    Signed-off-by: insane-22 <bhumikabachchan11315@gmail.com>

--- packages/jaeger-ui/src/components/DeepDependencies/Graph/DdgNodeContent/index.tsx
+++ packages/jaeger-ui/src/components/DeepDependencies/Graph/DdgNodeContent/index.tsx
@@ -160,7 +160,7 @@ export const UnconnectedDdgNodeContent = (props: TProps) => {
     if (decorationID) {
       getDecoration(decorationID, service, typeof operation === 'string' ? operation : undefined);
     }
-  }, [decorationID, getDecoration, operation, service]);
+  }, [decorationID, getDecoration]);
 
   React.useEffect(() => {
     return () => {
@@ -169,7 +169,7 @@ export const UnconnectedDdgNodeContent = (props: TProps) => {
         hoveredIndices.current.clear();
       }
     };
-  }, [setViewModifier]);
+  }, []);
 
   const checkTooltipPosition = React.useCallback(() => {
     if (!nodeRef.current) return;
