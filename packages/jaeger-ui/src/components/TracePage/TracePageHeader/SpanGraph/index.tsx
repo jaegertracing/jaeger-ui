e2a8196e67102a925f12c38e875b8df6ab95bbec
thc1006 <84045975+thc1006@users.noreply.github.com>
2026-01-15 23:32:26 +0800

    fix: remove redundant React import in SpanGraph
    Since memo and useMemo are imported from react on line 5, the
    wildcard import '* as React' was unnecessary and has been removed
    to follow consistent import patterns.
    
    Signed-off-by: thc1006 <84045975+thc1006@users.noreply.github.com>

--- packages/jaeger-ui/src/components/TracePage/TracePageHeader/SpanGraph/index.tsx
+++ packages/jaeger-ui/src/components/TracePage/TracePageHeader/SpanGraph/index.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2017 Uber Technologies, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import * as React from 'react';
 import { memo, useMemo } from 'react';
 
 import CanvasSpanGraph from './CanvasSpanGraph';
