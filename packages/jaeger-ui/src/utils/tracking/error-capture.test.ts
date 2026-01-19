b0b7b9e8ba98f1c45bbc1ed3948e1d2fbf1258dc
Yuri Shkuro <github@ysh.us>
2026-01-18 22:38:10 -0500

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- packages/jaeger-ui/src/utils/tracking/error-capture.test.ts
+++ packages/jaeger-ui/src/utils/tracking/error-capture.test.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
 import { init, captureException, formatErrorMessage, formatStackTrace } from './error-capture';
 
 describe('error-capture', () => {
