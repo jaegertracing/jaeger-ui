8475c5525bb10a2dfb604f1e9469fc0c671dc67b
harsh kumar <135993950+hxrshxz@users.noreply.github.com>
2026-01-19 05:48:27 +0530

    refactor: convert HopsSelector Selector to functional component (#3340)
    ## Which problem is this PR solving?
    - Part of #2610
    ## Description of the changes
    - Converts `Selector` (in HopsSelector) from `React.PureComponent` to a
    functional component using `useCallback`
    ## How was this change tested?
    - `npm run tsc-lint` passes
    - `npm test` passes (existing tests)
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [ ] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
    
    Signed-off-by: Harsh <harshmastic@gmail.com>
    Signed-off-by: greedy-wudpeckr <mudituiet@gmail.com>

--- packages/jaeger-ui/src/components/DeepDependencies/Header/HopsSelector/Selector.tsx
+++ packages/jaeger-ui/src/components/DeepDependencies/Header/HopsSelector/Selector.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2019 Uber Technologies, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { PureComponent } from 'react';
+import React, { memo, useCallback } from 'react';
 import { Popover } from 'antd';
 import { ImSortAmountAsc } from 'react-icons/im';
 import { IoChevronForward } from 'react-icons/io5';
@@ -23,100 +23,108 @@ type TProps = {
 
 const CLASSNAME = 'HopsSelector--Selector';
 
-export default class Selector extends PureComponent<TProps> {
-  private handleClick(distance: number) {
-    const { direction, furthestFullDistance, handleClick } = this.props;
-    handleClick(distance, direction);
-    trackHopChange(furthestFullDistance, distance, direction);
+function Selector(props: TProps) {
+  const { direction, furthestDistance, furthestFullDistance, furthestFullness, handleClick, hops } = props;
+
+  const handleBtnClick = useCallback(
+    (distance: number) => {
+      handleClick(distance, direction);
+      trackHopChange(furthestFullDistance, distance, direction);
+    },
+    [direction, furthestFullDistance, handleClick]
+  );
+
+  const makeBtn = useCallback(
+    (
+      { distance, fullness, suffix = 'popover-content' }: THop & { suffix?: string },
+      showChevron?: number
+    ) => {
+      const testIdSuffix = `${direction === EDirection.Downstream ? 'down' : 'up'}-${Math.abs(distance)}`;
+      return (
+        <React.Fragment key={`${distance} ${direction} ${suffix}`}>
+          {Boolean(showChevron) && (
+            <IoChevronForward className={`${CLASSNAME}--ChevronRight is-${fullness}`} />
+          )}
+          <button
+            className={`${CLASSNAME}--btn is-${fullness} ${CLASSNAME}--${suffix}`}
+            type="button"
+            onClick={() => handleBtnClick(distance)}
+            data-testid={`hop-${testIdSuffix}${suffix === 'popover-content' ? '-popover' : ''}`}
+          >
+            {Math.abs(distance)}
+          </button>
+        </React.Fragment>
+      );
+    },
+    [direction, handleBtnClick]
+  );
+
+  const streamText = direction === EDirection.Downstream ? 'Down' : 'Up';
+  const streamLabel = `${streamText}stream hops`;
+  const lowercaseLabel = streamLabel.toLowerCase();
+
+  if (hops.length <= 1) {
+    return <span className={CLASSNAME}>No {lowercaseLabel}</span>;
   }
 
-  private makeBtn = (
-    { distance, fullness, suffix = 'popover-content' }: THop & { suffix?: string },
-    showChevron?: number
-  ) => {
-    const { direction } = this.props;
-    const testIdSuffix = `${direction === EDirection.Downstream ? 'down' : 'up'}-${Math.abs(distance)}`;
-    return (
-      <React.Fragment key={`${distance} ${direction} ${suffix}`}>
-        {Boolean(showChevron) && <IoChevronForward className={`${CLASSNAME}--ChevronRight is-${fullness}`} />}
-        <button
-          className={`${CLASSNAME}--btn is-${fullness} ${CLASSNAME}--${suffix}`}
-          type="button"
-          onClick={() => this.handleClick(distance)}
-          data-testid={`hop-${testIdSuffix}${suffix === 'popover-content' ? '-popover' : ''}`}
-        >
-          {Math.abs(distance)}
-        </button>
-      </React.Fragment>
-    );
-  };
+  const decrementBtn = (
+    <button
+      key={`decrement ${direction}`}
+      disabled={furthestDistance === 0}
+      className={`${CLASSNAME}--decrement`}
+      type="button"
+      onClick={() => handleClick(furthestDistance - direction, direction)}
+      data-testid={`decrement-${direction === EDirection.Downstream ? 'down' : 'up'}`}
+    >
+      -
+    </button>
+  );
+  const incrementBtn = (
+    <button
+      key={`increment ${direction}`}
+      disabled={furthestDistance === hops[hops.length - 1].distance}
+      className={`${CLASSNAME}--increment`}
+      type="button"
+      onClick={() => handleClick(furthestDistance + direction, direction)}
+      data-testid={`increment-${direction === EDirection.Downstream ? 'down' : 'up'}`}
+    >
+      +
+    </button>
+  );
+  const delimiterBtn = makeBtn({
+    ...hops[hops.length - 1],
+    suffix: 'delimiter',
+  });
+  const furthestBtn = makeBtn({
+    distance: furthestDistance,
+    fullness: furthestFullness,
+    suffix: 'furthest',
+  });
 
-  render() {
-    const { direction, furthestDistance, furthestFullness, handleClick, hops } = this.props;
-    const streamText = direction === EDirection.Downstream ? 'Down' : 'Up';
-    const streamLabel = `${streamText}stream hops`;
-    const lowercaseLabel = streamLabel.toLowerCase();
-    if (hops.length <= 1) {
-      return <span className={CLASSNAME}>No {lowercaseLabel}</span>;
-    }
-
-    const decrementBtn = (
-      <button
-        key={`decrement ${direction}`}
-        disabled={furthestDistance === 0}
-        className={`${CLASSNAME}--decrement`}
-        type="button"
-        onClick={() => handleClick(furthestDistance - direction, direction)}
-        data-testid={`decrement-${direction === EDirection.Downstream ? 'down' : 'up'}`}
+  return (
+    <Popover
+      arrow={{ pointAtCenter: true }}
+      content={
+        <div data-testid={`popover-content-${direction === EDirection.Downstream ? 'down' : 'up'}`}>
+          {[decrementBtn, ...hops.map(makeBtn), incrementBtn]}
+        </div>
+      }
+      placement="bottom"
+      title={`Visible ${lowercaseLabel}`}
+    >
+      <span
+        className={CLASSNAME}
+        data-testid={`hops-selector-${direction === EDirection.Downstream ? 'down' : 'up'}`}
       >
-        -
-      </button>
-    );
-    const incrementBtn = (
-      <button
-        key={`increment ${direction}`}
-        disabled={furthestDistance === hops[hops.length - 1].distance}
-        className={`${CLASSNAME}--increment`}
-        type="button"
-        onClick={() => handleClick(furthestDistance + direction, direction)}
-        data-testid={`increment-${direction === EDirection.Downstream ? 'down' : 'up'}`}
-      >
-        +
-      </button>
-    );
-    const delimiterBtn = this.makeBtn({
-      ...hops[hops.length - 1],
-      suffix: 'delimiter',
-    });
-    const furthestBtn = this.makeBtn({
-      distance: furthestDistance,
-      fullness: furthestFullness,
-      suffix: 'furthest',
-    });
-
-    return (
-      <Popover
-        arrow={{ pointAtCenter: true }}
-        content={
-          <div data-testid={`popover-content-${direction === EDirection.Downstream ? 'down' : 'up'}`}>
-            {[decrementBtn, ...hops.map(this.makeBtn), incrementBtn]}
-          </div>
-        }
-        placement="bottom"
-        title={`Visible ${lowercaseLabel}`}
-      >
-        <span
-          className={CLASSNAME}
-          data-testid={`hops-selector-${direction === EDirection.Downstream ? 'down' : 'up'}`}
-        >
-          <ImSortAmountAsc className={`${CLASSNAME}--AscIcon is-${streamText}`} />
-          {streamLabel}
-          {furthestBtn}
-          <span className={`${CLASSNAME}--delimiter`}>/</span>
-          {delimiterBtn}
-          <ChevronDown className="ub-ml1" />
-        </span>
-      </Popover>
-    );
-  }
+        <ImSortAmountAsc className={`${CLASSNAME}--AscIcon is-${streamText}`} />
+        {streamLabel}
+        {furthestBtn}
+        <span className={`${CLASSNAME}--delimiter`}>/</span>
+        {delimiterBtn}
+        <ChevronDown className="ub-ml1" />
+      </span>
+    </Popover>
+  );
 }
+
+export default memo(Selector);
