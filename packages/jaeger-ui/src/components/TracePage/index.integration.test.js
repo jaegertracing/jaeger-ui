edb1b4c77a6eb1b2044f5d20fc59bfcb56170993
Samar <hello.samar7@gmail.com>
2026-02-03 19:56:30 +0000

    test: added integration test for tracePage uppercase to lowercase trace ID normalization
    Signed-off-by: Samar <hello.samar7@gmail.com>

---
+++ packages/jaeger-ui/src/components/TracePage/index.integration.test.js
@@ -0,0 +1,61 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom-v5-compat', () => ({
+  ...jest.requireActual('react-router-dom-v5-compat'),
+  useNavigate: () => mockNavigate,
+  useLocation: () => ({ search: '', state: null }),
+}));
+
+jest.mock('../../hooks/useConfig', () => ({
+  useConfig: () => ({
+    archiveEnabled: false,
+    storageCapabilities: null,
+    criticalPathEnabled: false,
+    disableJsonView: false,
+    traceGraph: null,
+    useOpenTelemetryTerms: false,
+  }),
+}));
+
+jest.mock('../../utils/withRouteProps', () => ({
+  __esModule: true,
+  default: Component => Component,
+}));
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  connect: () => () => () => null,
+}));
+
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import TracePage from './index';
+
+describe('TracePage URL normalization', () => {
+  beforeEach(() => {
+    mockNavigate.mockClear();
+  });
+
+  it('normalizes uppercase trace IDs to lowercase in the URL', async () => {
+    const uppercaseTraceId = 'ABC123DEF456';
+    const lowercaseTraceId = uppercaseTraceId.toLowerCase();
+
+    render(
+      <MemoryRouter>
+        <TracePage params={{ id: uppercaseTraceId }} />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith(
+        expect.stringContaining(lowercaseTraceId),
+        expect.objectContaining({ replace: true })
+      );
+    });
+  });
+});
