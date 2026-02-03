3dd136ac85c676301d0830b9279a77752268ec78
蔡秀吉 <84045975+thc1006@users.noreply.github.com>
2026-02-03 19:58:48 +0800

    Update packages/plexus/src/Digraph/Nodes.tsx
    Co-authored-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>
    Signed-off-by: 蔡秀吉 <84045975+thc1006@users.noreply.github.com>

--- packages/plexus/src/Digraph/Nodes.tsx
+++ packages/plexus/src/Digraph/Nodes.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) 2026 The Jaeger Authors.
 // Copyright (c) 2019 Uber Technologies, Inc.
+// Copyright (c) 2019 Uber Technologies, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 import * as React from 'react';
