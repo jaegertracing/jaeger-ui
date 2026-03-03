70391fe533418aed6234fb0978b5a15b75e2cf1a
gulshank0 <gulshanbahadur002@gmail.com>
2026-03-03 16:53:10 +0530

    using useLayoutEffect instead of the useEffect to prevent flicker and Measuring DOM size
    Signed-off-by: gulshank0 <gulshanbahadur002@gmail.com>

--- packages/jaeger-ui/src/components/Monitor/ServicesView/index.tsx
+++ packages/jaeger-ui/src/components/Monitor/ServicesView/index.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 The Jaeger Authors.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef, useCallback, useLayoutEffect } from 'react';
 import { Row, Col, Input, Alert, Select } from 'antd';
 import { ActionFunctionAny, Action } from 'redux-actions';
 import _debounce from 'lodash/debounce';
@@ -226,7 +226,7 @@ export function MonitorATMServicesViewImpl(props: TProps) {
 
   // Recalculate graph width once services finish loading, since the graph
   // container ref is not mounted while the loading spinner is shown (#3539).
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!servicesLoading) {
       updateDimensions();
     }
