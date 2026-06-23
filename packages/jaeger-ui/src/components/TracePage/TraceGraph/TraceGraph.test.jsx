a35627230218ff0cfd7d4febeabd4d7ebd177d9c
Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>
2026-06-23 12:26:20 +0530

    test(tracegraph): update button queries to use new aria-labels
    Signed-off-by: Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>

--- packages/jaeger-ui/src/components/TracePage/TraceGraph/TraceGraph.test.jsx
+++ packages/jaeger-ui/src/components/TracePage/TraceGraph/TraceGraph.test.jsx
@@ -95,9 +95,9 @@ describe('<TraceGraph>', () => {
 
     // Initial mode should be service
     expect(screen.getByTestId('mock-digraph')).toHaveAttribute('data-mode', MODE_SERVICE);
-    const timeButton = screen.getByRole('button', { name: 'T' });
-    const selftimeButton = screen.getByRole('button', { name: 'ST' });
-    const serviceButton = screen.getByRole('button', { name: 'S' });
+    const timeButton = screen.getByRole('button', { name: 'Color by total time' });
+    const selftimeButton = screen.getByRole('button', { name: 'Color by self time' });
+    const serviceButton = screen.getByRole('button', { name: 'Color by service' });
 
     // Switch to time
     await userEvent.click(timeButton);
