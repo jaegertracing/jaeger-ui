4d83c93ee71aef488e7520219baa97c13e19c704
Akshat Singhai <akshatsinghai6682@gmail.com>
2026-06-28 03:56:19 +0530

    revert: remove accidental change to order-by.ts
    Signed-off-by: Akshat Singhai <akshatsinghai6682@gmail.com>

--- packages/jaeger-ui/src/model/order-by.ts
+++ packages/jaeger-ui/src/model/order-by.ts
@@ -6,4 +6,3 @@ export const LONGEST_FIRST = 'LONGEST_FIRST';
 export const SHORTEST_FIRST = 'SHORTEST_FIRST';
 export const MOST_SPANS = 'MOST_SPANS';
 export const LEAST_SPANS = 'LEAST_SPANS';
-export const OLDEST_FIRST = 'OLDEST_FIRST';
