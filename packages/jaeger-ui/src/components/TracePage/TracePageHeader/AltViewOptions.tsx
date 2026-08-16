01e2075fd48e1b4f27ba2f8d0f995badf27c20a2
Shaurya Mishra <devm89548@gmail.com>
2026-08-16 10:58:57 +0530

    fix(ui): stop arrow keys in trace view dropdowns from resizing minimap
    Fixes #4178
    
    Signed-off-by: Shaurya Mishra <devm89548@gmail.com>

--- packages/jaeger-ui/src/components/TracePage/TracePageHeader/AltViewOptions.tsx
+++ packages/jaeger-ui/src/components/TracePage/TracePageHeader/AltViewOptions.tsx
@@ -79,7 +79,7 @@ export default function AltViewOptions(props: Props) {
   const currentItem = MENU_ITEMS.find(item => item.viewType === viewType);
   const dropdownText = currentItem ? currentItem.label : 'Alternate Views';
   return (
-    <Dropdown menu={{ items: dropdownItems }} trigger={['click']}>
+    <Dropdown menu={{ items: dropdownItems, onKeyDown: e => e.stopPropagation() }} trigger={['click']}>
       <Button className="AltViewOptions">
         {`${dropdownText} `}
         <IoChevronDown />
